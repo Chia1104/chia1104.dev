d5aba03f369fb3989fec10f75de9453e37fc0127
Chia1104 <38397958+Chia1104@users.noreply.github.com>
2022-04-25 23:32:51 +0800

    :heavy_plus_sign: Add Framer Motion

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { motion } from "framer-motion";
 
-function ChiaWEB({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function ChiaWEB({ Component, pageProps, router }: AppProps) {
+  return (
+      <motion.div
+          key={router.route}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.5 }}
+      >
+        <Component {...pageProps} />
+      </motion.div>
+  )
 }
 
 export default ChiaWEB
--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,16 +1,47 @@
 import type { NextPage } from 'next'
+import { motion } from 'framer-motion'
 import { Header } from "../../components/Header";
 import { Footer } from "../../components/Footer";
+import { useRouter } from "next/router";
 
 const HomePage: NextPage = () => {
+    const router = useRouter()
+
     return (
         <div className="c-container">
             <Header />
 
             <main className="main">
-                <h1 className="title">
-                    HOME Page
-                </h1>
+                <motion.div
+                    initial="hidden"
+                    animate="visible"
+                    variants={{
+                        hidden: {
+                            opacity: 0,
+                            y: -100
+                        },
+                        visible: {
+                            opacity: 1,
+                            y: 0
+                        }
+                    }}
+                >
+                    <h1 className="title text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+                        HOME Page
+                    </h1>
+                </motion.div>
+                <button type="button" onClick={() => router.push('/post')}>
+                    Post
+                </button>
+                <button type="button" onClick={() => router.push('/result')}>
+                    Result
+                </button>
+                <article>
+                    <h1>Hello Next.js</h1>
+                    <p>
+                        This is the content of the page.
+                    </p>
+                </article>
             </main>
 
             <Footer />
