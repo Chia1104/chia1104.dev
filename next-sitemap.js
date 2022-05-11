1edaaf016c7090b7dd35a666db48ce1caf3c416e
Chia1104 <38397958+Chia1104@users.noreply.github.com>
2022-05-11 15:39:13 +0800

    :mag: Improve SEO.

---
+++ next-sitemap.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+    siteUrl: process.env.SITE_URL || 'https://www.chia1104.dev',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+}
