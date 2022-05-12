83e45abe7cc125bb927fb05d70fc852ac5a3b826
yuyuc <yuyuchia7423@gmail.com>
2022-05-12 16:44:55 +0800

    :test_tube: Testing code area.

--- src/components/pages/posts/MDXComponents/MDXCode/MDXCode.tsx
+++ src/components/pages/posts/MDXComponents/MDXCode/MDXCode.tsx
@@ -1,7 +1,7 @@
 
 export const MDXCode = (props: any) => {
     return (
-        <div className="bg-code w-full my-3 p-2 rounded-xl text-white">
+        <div className="bg-code w-full my-5 p-2 rounded-xl text-white overflow-x-auto">
             {props.children}
         </div>
     );
