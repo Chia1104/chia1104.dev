1d44ce9c4c37052ccf65a8863dc24aaaeb75215a
yuyuc <yuyuchia7423@gmail.com>
2022-05-15 17:10:41 +0800

    :art: Add some style.

--- src/components/pages/posts/MDXComponents/MDXCode/MDXCode.tsx
+++ src/components/pages/posts/MDXComponents/MDXCode/MDXCode.tsx
@@ -1,27 +1,11 @@
 
-export const MDXCode = (props: any, type: string, msg: string) => {
-    type = 'info'
+export const MDXCode = (props: any) => {
 
     return (
         <div className="relative">
-            {
-                type === 'info' &&
-                <div className="bg-info/70 -top-4 absolute border-2 border-info rounded-full px-3 py-1 text-white">
-                    {msg || 'info'}
-                </div>
-            }
-            {
-                type === 'warning' &&
-                <div className="bg-warning/70 -top-4 absolute border-2 border-warning rounded-full px-3 py-1 text-white">
-                    {msg|| 'warning'}
-                </div>
-            }
-            {
-                type === 'error' &&
-                <div className="bg-danger/70 -top-4 absolute border-2 border-danger rounded-full px-3 py-1 text-white">
-                    {msg || 'error'}
-                </div>
-            }
+            <div className="bg-info/70 -top-4 absolute border-2 border-info rounded-full px-3 py-1 text-white">
+                info
+            </div>
             <div className="bg-code w-full my-7 p-2 pt-7 rounded-xl text-white overflow-x-auto">
                 {props.children}
             </div>
