8360c1a99e77c688d78196e10bc7d8c82322290e
yuyuc <yuyuchia7423@gmail.com>
2022-06-05 01:15:56 +0800

    :test_tube: Testing recaptcha

--- src/components/globals/ActionIcon/Contact/Contact.tsx
+++ src/components/globals/ActionIcon/Contact/Contact.tsx
@@ -6,9 +6,10 @@ import {useAppDispatch} from "@chia/src/hooks/useAppDispatch";
 import {activeActionIconSheet, selectActionIconSheet} from "@chia/store/modules/ActionSheet/actionSheet.slice";
 import { motion } from "framer-motion"
 import {useAppSelector} from "@chia/src/hooks/useAppSelector";
+import Script from "next/script";
 
 const outside = {
-    open: { opacity: 1, height: '600px', width: '350px' },
+    open: { opacity: 1, height: '550px', width: '330px' },
     closed: { opacity: 0, height: 0, width: 0, transition: { delay: 0.2 } },
 }
 const inside = {
@@ -63,7 +64,7 @@ export const Contact: FC = () => {
                     <h2 className="text-xl pb-2">Message</h2>
                     <textarea
                         name="message"
-                        className="w-full h-30 p-2 border-2 border-primary rounded-lg mb-4"
+                        className="w-full max-h-36 p-2 border-2 border-primary rounded-lg mb-4 overflow-y-auto"
                         placeholder={'Your message'}
                         required
                     />
@@ -80,20 +81,24 @@ export const Contact: FC = () => {
                     >
                         Send
                     </button>
+                    <Script
+                        src="https://www.google.com/recaptcha/api.js"
+                    />
+                    <div className="g-recaptcha self-center my-3" data-sitekey={process.env.NEXT_PUBLIC_RE_CAPTCHA_KEY} />
                     <ValidationError errors={state.errors} className="text-warning mt-2"/>
                     {
                         state.succeeded && <p className="text-success  mt-2">You have sent the email successfully</p>
                     }
                 </form>
-                <div className="mt-auto mb-5">
-                    <Image
-                        src="/memoji/contact-memoji.PNG"
-                        alt={"Contact Memoji"}
-                        width={180}
-                        height={180}
-                        aria-label={"Contact Memoji"}
-                    />
-                </div>
+                {/*<div className="mt-auto mb-5">*/}
+                {/*    <Image*/}
+                {/*        src="/memoji/contact-memoji.PNG"*/}
+                {/*        alt={"Contact Memoji"}*/}
+                {/*        width={180}*/}
+                {/*        height={180}*/}
+                {/*        aria-label={"Contact Memoji"}*/}
+                {/*    />*/}
+                {/*</div>*/}
             </motion.div>
         </motion.div>
     )
