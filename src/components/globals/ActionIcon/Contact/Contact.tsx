0af1a710f457e7ba7c76f6852182869c0857ac08
yuyuc <yuyuchia7423@gmail.com>
2022-06-05 02:31:19 +0800

    :sparkles: Add Google reCAPTCHA.

--- src/components/globals/ActionIcon/Contact/Contact.tsx
+++ src/components/globals/ActionIcon/Contact/Contact.tsx
@@ -91,12 +91,6 @@ export const Contact: FC = () => {
                     {
                         state.succeeded && <p className="text-success  mt-2">You have sent the email successfully</p>
                     }
-                    {
-                        state.errors.length > 0 &&
-                        <p className="text-warning mt-2">
-                            Make sure you have filled all the fields correctly
-                        </p>
-                    }
                 </form>
                 {/*<div className="mt-auto mb-5">*/}
                 {/*    <Image*/}
