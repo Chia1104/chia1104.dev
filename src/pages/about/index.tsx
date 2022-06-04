718bd19be58966a9f13f7e1f5e1173155297f17c
yuyuc <yuyuchia7423@gmail.com>
2022-06-04 17:29:03 +0800

    :adhesive_bandage: Simple fix for a non-critical issue.

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -2,14 +2,14 @@ import type { NextPage } from 'next'
 import { Layout } from "@chia/components/globals/Layout";
 import { Chia } from"@chia/utils/meta/chia"
 import {Resume} from "@chia/components/pages/about/Resume";
-import type {GetServerSideProps} from "next";
+import type {GetStaticProps} from "next";
 import {getImage} from "@chia/lib/firebase/files/services";
 
 interface Props {
     url: string,
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
     const avatarUrl = await getImage('me/me.JPG');
 
     return {
