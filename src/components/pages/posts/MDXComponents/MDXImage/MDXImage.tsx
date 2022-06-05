eaf3c835cd55cd7386ab8537b20fab7ffa32c140
yuyuc <yuyuchia7423@gmail.com>
2022-06-06 04:21:53 +0800

    :memo: Writing my first post

--- src/components/pages/posts/MDXComponents/MDXImage/MDXImage.tsx
+++ src/components/pages/posts/MDXComponents/MDXImage/MDXImage.tsx
@@ -10,14 +10,14 @@ export const MDXImage: FC<Props> = (props) => {
 
     return (
         <div className="flex flex-col justify-center items-center my-5">
-            <div className="aspect-w-2 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200">
+            <div className="aspect-w-2 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 shadow-lg">
                 <Image
                     src={props.s}
                     alt={props.a}
                     aria-label={props.a}
                     blurDataURL={'/loader/skeleton.gif'}
                     placeholder="blur"
-                    className="rounded-lg"
+                    className="rounded-lg hover:scale-[1.05] duration-300 transition ease-in-out"
                     loading="lazy"
                     objectFit="cover"
                     layout="fill"
@@ -25,7 +25,7 @@ export const MDXImage: FC<Props> = (props) => {
                     {...props}
                 />
             </div>
-            <p className="self-center mt-1">{props.a || ''}</p>
+            <p className="self-center mt-2">{props.a || ''}</p>
         </div>
     );
 }
