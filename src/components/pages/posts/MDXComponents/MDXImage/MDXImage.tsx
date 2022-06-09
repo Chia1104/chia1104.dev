afc26de2cdc90e3eb0758f8a327417664806c6d2
yuyuc <yuyuchia7423@gmail.com>
2022-06-09 18:18:10 +0800

    :adhesive_bandage: Fix some layout.

--- src/components/pages/posts/MDXComponents/MDXImage/MDXImage.tsx
+++ src/components/pages/posts/MDXComponents/MDXImage/MDXImage.tsx
@@ -1,27 +1,24 @@
 import Image from 'next/image';
 import { FC } from 'react';
-import cx from 'classnames';
 
 interface Props {
-    imgAlt: string;
-    imgSrc: string;
-    aspectWidth?: number;
-    aspectHeight?: number;
+    img_alt: string;
+    img_src: string;
+    aspect_width?: number;
+    aspect_height?: number;
 }
 
 export const MDXImage: FC<Props> = (props) => {
-    const w = props.aspectWidth ? props.aspectWidth : 2;
-    const h = props.aspectHeight ? props.aspectHeight : 1;
+    const w = props.aspect_width || 2;
+    const h = props.aspect_height || 1;
 
     return (
         <div className="flex flex-col justify-center items-center my-5">
-            <div className={cx('w-full overflow-hidden rounded-lg bg-gray-200 shadow-lg',
-                `aspect-w-${w} aspect-h-${h}`
-            )}>
+            <div className={`aspect-w-${w} aspect-h-${h} w-full overflow-hidden rounded-lg bg-gray-200 shadow-lg`}>
                 <Image
-                    src={props.imgSrc}
-                    alt={props.imgAlt}
-                    aria-label={props.imgAlt}
+                    src={props.img_src}
+                    alt={props.img_alt}
+                    aria-label={props.img_alt}
                     blurDataURL={'/loader/skeleton.gif'}
                     placeholder="blur"
                     className="rounded-lg hover:scale-[1.05] duration-300 transition ease-in-out"
@@ -32,7 +29,7 @@ export const MDXImage: FC<Props> = (props) => {
                     {...props}
                 />
             </div>
-            <p className="self-center mt-2">{props.imgAlt || ''}</p>
+            <p className="self-center mt-2">{props.img_alt || ''}</p>
         </div>
     );
 }
