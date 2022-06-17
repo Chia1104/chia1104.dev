b89d88baa9b718434d76d4471b5394b3a66136a1
Chia1104 <38397958+Chia1104@users.noreply.github.com>
2022-06-17 12:26:36 +0800

    :sparkles: Complete Modal.

--- src/components/globals/Modal/Modal.tsx
+++ src/components/globals/Modal/Modal.tsx
@@ -1,26 +1,47 @@
-import { FC } from "react";
-import { motion } from "framer-motion";
+import { FC, ReactNode } from "react";
+import {AnimatePresence, motion} from "framer-motion";
 
 interface Props {
     isShow: boolean;
-    children: React.ReactNode;
+    children: ReactNode;
     handleClose: () => void;
+    className?: string;
 }
 
-export const Modal: FC<Props> = ({ children, isShow, handleClose }) => {
-    const v = {
-        open: { opacity: 1, delay: 0.01, duration: 300 },
-        closed: { opacity: 0 },
+export const Modal: FC<Props> = (props) => {
+    const ov = {
+        open: { opacity: 1 },
+        closed: { opacity: 0, delay: 300 },
+    }
+    const iv = {
+        open: { opacity: 1, y: 0 },
+        closed: { opacity: 0, y: -100 },
     }
 
     return (
-        <motion.div
-            onClick={handleClose}
-            initial={'closed'}
-            animate={ isShow ? 'open' : 'closed' }
-            variants={v}
-            className="modal">
-            {children}
-        </motion.div>
+        <AnimatePresence>
+            {
+                props.isShow && (
+                    <motion.div
+                        onClick={props.handleClose}
+                        initial={'closed'}
+                        animate={ props.isShow ? 'open' : 'closed' }
+                        exit={'closed'}
+                        variants={ov}
+                        className="modal">
+                        <motion.div
+                            initial={'closed'}
+                            animate={ props.isShow ? 'open' : 'closed' }
+                            exit={'closed'}
+                            variants={iv}
+                            // className="w-[300px]"
+                            {...props}
+                        >
+                            {props.children}
+                        </motion.div>
+                    </motion.div>
+                )
+            }
+        </AnimatePresence>
     )
 }
--- src/components/pages/portfolios/Design/DMPoster/DMPoster.tsx
+++ src/components/pages/portfolios/Design/DMPoster/DMPoster.tsx
@@ -2,8 +2,8 @@ import { FC, useRef, useState } from "react";
 import useHover from "@chia/src/hooks/useHover";
 import Image from "next/image";
 import cx from "classnames";
-import { motion } from "framer-motion";
-// import {Modal} from "@chia/components/globals/Modal";
+import {Modal} from "@chia/components/globals/Modal";
+import {HoverButton} from "@chia/components/globals/HoverButton";
 
 interface Props {
     url: string;
@@ -35,36 +35,29 @@ export const DMPoster: FC<Props> = ({ url }) => {
                 layout="fill"
                 quality={100}
             />
-            <motion.div
-                initial={'closed'}
-                animate={isHover ? 'open' : 'closed'}
-                variants={v}
-                className="w-full h-full bg-gradient-to-b from-gray-600/70 to-gray-600/0 absolute bottom-0 left-0 flex justify-center items-center">
-                <button
-                    onClick={() => setIsShow(!isShow)}
-                >
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-16 w-16 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0zM10 7v3m0 0v3m0-3h3m-3 0H7" />
-                    </svg>
-                </button>
-            </motion.div>
-            {/*{*/}
-            {/*    isShow && (*/}
-            {/*        <Modal*/}
-            {/*            isShow={isShow}*/}
-            {/*            handleClose={handleClose}*/}
-            {/*        >*/}
-            {/*            <img*/}
-            {/*                src={url}*/}
-            {/*                alt={'DMPoster'}*/}
-            {/*                aria-label={'DMPoster'}*/}
-            {/*                width={'100%'}*/}
-            {/*                height={'auto'}*/}
-            {/*                className="w-[75%] max-w-[750px]"*/}
-            {/*            />*/}
-            {/*        </Modal>*/}
-            {/*    )*/}
-            {/*}*/}
+            <HoverButton
+                handleClick={() => setIsShow(!isShow)}
+                refTarget={r}
+            />
+            <Modal
+                isShow={isShow}
+                handleClose={handleClose}
+                className="w-[85%] max-w-[550px]"
+            >
+                <div className="w-full aspect-w-1 aspect-h-1">
+                    <Image
+                        src={url || '/posts/example-posts/example.jpg'}
+                        alt={'DMPoster'}
+                        aria-label={'DMPoster'}
+                        blurDataURL={'/loader/skeleton.gif'}
+                        placeholder="blur"
+                        loading="lazy"
+                        layout="fill"
+                        objectFit="contain"
+                        quality={100}
+                    />
+                </div>
+            </Modal>
         </div>
     )
 }
