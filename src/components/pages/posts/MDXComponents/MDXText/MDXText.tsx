7af23dd3708aa1e57cb846fc712333e13a1377ef
Chia1104 <yuyuchia7423@gmail.com>
2022-07-22 18:04:40 +0800

    :art: Fix some style.

--- src/components/pages/posts/MDXComponents/MDXText/MDXText.tsx
+++ src/components/pages/posts/MDXComponents/MDXText/MDXText.tsx
@@ -6,8 +6,12 @@ interface Props {
 
 export const MDXParagraph: FC<Props> = ({ children }) => {
   return <p className="text-base leading-loose">{children}</p>;
-}
+};
 
 export const MDXStrong: FC<Props> = ({ children }) => {
-  return <strong className="text-base leading-loose font-bold c-text-bg-info-half">{children}</strong>;
-}
+  return (
+    <strong className="text-base leading-loose font-bold c-text-bg-info-half">
+      {children}
+    </strong>
+  );
+};
