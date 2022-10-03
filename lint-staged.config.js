159904bd9ba6449c372359fa47bcd8b06d3c5b0e
Chia1104 <yuyuchia7423@gmail.com>
2022-10-03 13:10:15 +0800

    :construction_worker: Add Lint Staged.

---
+++ lint-staged.config.js
@@ -0,0 +1,26 @@
+const { quote } = require("shell-quote");
+const { ESLint } = require("eslint");
+
+const eslint = new ESLint();
+const isWin = process.platform === "win32";
+
+module.exports = {
+  "src/**/*.{js,jsx,ts,tsx}": (filenames) => {
+    const escapedFileNames = filenames
+      .map((filename) => `"${isWin ? filename : escape([filename])}"`)
+      .join(" ");
+    return [
+      `prettier --write ${escapedFileNames}`,
+      `eslint --fix ${filenames
+        .filter((file) => !eslint.isPathIgnored(file))
+        .map((f) => `"${f}"`)
+        .join(" ")}`,
+      `git add ${escapedFileNames}`,
+    ];
+  },
+};
+
+function escape(str) {
+  const escaped = quote(str);
+  return escaped.replace(/\\@/g, "@");
+}
