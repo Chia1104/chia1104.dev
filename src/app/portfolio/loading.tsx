4245dcf90a83eb904375c83b1788a0eabb8c1e1c
Chia1104 <yuyuchia7423@gmail.com>
2022-10-29 18:05:24 +0800

    :sparkles: Add design loader.

--- src/app/portfolio/loading.tsx
+++ src/app/portfolio/loading.tsx
@@ -4,26 +4,40 @@ import { Page } from "@chia/components/client";
 const Loading = () => {
   return (
     <Page>
-      <article className="main c-container">
-        <header className="title sm:self-start pt-10">
+      <div className="main c-container">
+        <div className="title sm:self-start pt-10">
           <span className="c-text-bg-sec-half dark:c-text-bg-primary-half">
             GitHub Repositories
           </span>
-        </header>
+        </div>
         <p className="c-description sm:self-start pb-7">
           What I currently work on
         </p>
         <ReposLoader />
         <hr className="my-10 c-border-primary border-t-2 w-full" />
-        <header className="title sm:self-start c-text-bg-sec-half dark:c-text-bg-primary-half">
+        <p className="title sm:self-start c-text-bg-sec-half dark:c-text-bg-primary-half">
           Youtube Playlists
-        </header>
+        </p>
         <p className="c-description sm:self-start pb-7">
           I have created a few video for my Youtube channel.
         </p>
         <VideoLoader />
         <hr className="my-10 c-border-primary border-t-2 w-full" />
-      </article>
+        <p className="title sm:self-start c-text-bg-sec-half dark:c-text-bg-primary-half">
+          Design
+        </p>
+        <p className="c-description sm:self-start pb-7">
+          Some of my design work
+        </p>
+        <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-10 px-10">
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => (
+            <div
+              key={item}
+              className="animate-pulse aspect-w-3 aspect-h-5 w-full overflow-hidden rounded-lg bg-gray-200 shadow-lg relative"
+            />
+          ))}
+        </div>
+      </div>
     </Page>
   );
 };
