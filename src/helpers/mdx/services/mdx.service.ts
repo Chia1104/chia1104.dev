46309a141edd06d0c7d54c4e0d52b7f6a9ae1e0c
Chia1104 <yuyuchia7423@gmail.com>
2022-11-02 00:49:57 +0800

    :test_tube: Testing.

--- src/helpers/mdx/services/mdx.service.ts
+++ src/helpers/mdx/services/mdx.service.ts
@@ -1,4 +1,4 @@
-import "server-only";
+// import "server-only";
 import path from "path";
 import type { PostFrontMatter, PostSource } from "@chia/shared/types";
 import { POSTS_PATH } from "@chia/shared/constants";
