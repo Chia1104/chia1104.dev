c6a64438fa6b941229deb415a7a5a74b4ca9a289
Chia1104 <yuyuchia7423@gmail.com>
2022-11-03 14:19:29 +0800

    :arrow_up: Upgrade dependencies.

--- src/components/client/PostsList/PostItem.tsx
+++ src/components/client/PostsList/PostItem.tsx
@@ -43,6 +43,7 @@ const PostItem: FC<Props> = ({ data, i }) => {
         </span>
       </p>
       <Link
+        prefetch={false}
         className="absolute top-0 bottom-0 right-0 left-0"
         href={{
           pathname: "/posts/[slug]/",
