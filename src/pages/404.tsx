78473a6969775939446fa05a7617c7016ce16c9c
Chia1104 <yuyuchia7423@gmail.com>
2022-11-04 10:17:40 +0800

    :adhesive_bandage: Fix?

---
+++ src/pages/404.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import "../styles/globals.css";
+
+function NotFoundError() {
+  return (
+    <div className="w-screen h-screen c-container flex flex-col justify-center items-center">
+      <h1 className="title text-warning">There is nothing here!</h1>
+      <Image
+        src="/error/error-memoji.png"
+        alt={"Error Memoji"}
+        width={200}
+        height={200}
+        priority
+        aria-label={"Error Memoji"}
+      />
+    </div>
+  );
+}
+
+export default NotFoundError;
