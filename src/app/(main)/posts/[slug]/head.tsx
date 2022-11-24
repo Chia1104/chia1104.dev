2f654a43ac0984dcdb7f6a449aaf246da3390a82
Chia1104 <yuyuchia7423@gmail.com>
2022-11-24 11:19:07 +0800

    :test_tube: Testing.

--- src/app/(main)/posts/[slug]/head.tsx
+++ src/app/(main)/posts/[slug]/head.tsx
@@ -1,13 +1,13 @@
 import { Head } from "@chia/components/server";
 import { Chia } from "@chia/shared/meta/chia";
-// import { getPost } from "@chia/helpers/mdx/services";
+import { getPost } from "@chia/helpers/mdx/services";
 
 const PostDetailHead = async ({ params }: { params: any }) => {
-  // const { frontMatter } = await getPost(params.slug);
+  const { frontMatter } = await getPost(params.slug);
   return (
     <Head
-      title={`${params?.slug} | ${Chia.name} ${Chia.chineseName}`}
-      description={`${params?.slug}`}
+      title={`${frontMatter.title} | ${Chia.name} ${Chia.chineseName}`}
+      description={`${frontMatter.excerpt}`}
       type="article"
     />
   );
