c1b91dbeff783a5095005d13792c4159f394c291
Chia1104 <yuyuchia7423@gmail.com>
2022-11-24 12:12:53 +0800

    :recycle: Refactor code.

--- src/app/(main)/posts/[slug]/head.tsx
+++ src/app/(main)/posts/[slug]/head.tsx
@@ -3,14 +3,18 @@ import { Chia } from "@chia/shared/meta/chia";
 import { getPost } from "@chia/helpers/mdx/services";
 
 const PostDetailHead = async ({ params }: { params: any }) => {
-  const { frontMatter } = await getPost(params.slug);
-  return (
-    <Head
-      title={`${frontMatter.title} | ${Chia.name} ${Chia.chineseName}`}
-      description={`${frontMatter.excerpt}`}
-      type="article"
-    />
-  );
+  try {
+    const { frontMatter } = await getPost(params.slug);
+    return (
+      <Head
+        title={`${frontMatter.title} | ${Chia.name} ${Chia.chineseName}`}
+        description={`${frontMatter.excerpt}`}
+        type="article"
+      />
+    );
+  } catch (error) {
+    return <Head />;
+  }
 };
 
 export default PostDetailHead;
