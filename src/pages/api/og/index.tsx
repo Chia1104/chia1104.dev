bd2c8f370768dd2f83a077caf17edde0c7fa8e32
Chia1104 <yuyuchia7423@gmail.com>
2023-02-14 21:58:57 +0800

    :adhesive_bandage: Update og api.

--- src/pages/api/og/index.tsx
+++ src/pages/api/og/index.tsx
@@ -6,77 +6,70 @@ export const config = {
 };
 
 export default function handler(req: NextRequest) {
-  switch (req.method) {
-    case "GET":
-      try {
-        const { searchParams } = new URL(req.url);
-        const hasTitle = searchParams.has("title");
-        const title = hasTitle
-          ? searchParams.get("title")?.slice(0, 100)
-          : "Full Stack Engineer";
-        return new ImageResponse(
-          (
-            <div
-              tw="bg-slate-900"
+  try {
+    const { searchParams } = new URL(req.url);
+    const hasTitle = searchParams.has("title");
+    const title = hasTitle
+      ? searchParams.get("title")?.slice(0, 100)
+      : "Full Stack Engineer";
+    return new ImageResponse(
+      (
+        <div
+          tw="bg-slate-900"
+          style={{
+            backgroundSize: "150px 150px",
+            height: "100%",
+            width: "100%",
+            display: "flex",
+            textAlign: "center",
+            alignItems: "center",
+            justifyContent: "center",
+            flexDirection: "column",
+            flexWrap: "nowrap",
+          }}>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              justifyItems: "center",
+              position: "relative",
+            }}>
+            <img
               style={{
-                backgroundSize: "150px 150px",
-                height: "100%",
-                width: "100%",
-                display: "flex",
-                textAlign: "center",
-                alignItems: "center",
-                justifyContent: "center",
-                flexDirection: "column",
-                flexWrap: "nowrap",
-              }}>
-              <div
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  justifyItems: "center",
-                  position: "relative",
-                }}>
+                filter: "grayscale(45%) blur(2px)",
+              }}
+              tw="w-full"
+              alt="og"
+              src="https://firebasestorage.googleapis.com/v0/b/chia1104.appspot.com/o/images%2Fog-img.png?alt=media"
+            />
+            <div tw="flex absolute w-full justify-center items-center p-20">
+              <div tw="flex flex-col justify-center items-center mr-10">
                 <img
                   style={{
-                    filter: "grayscale(45%) blur(2px)",
+                    objectFit: "contain",
                   }}
-                  tw="w-full"
-                  alt="og"
-                  src="https://firebasestorage.googleapis.com/v0/b/chia1104.appspot.com/o/images%2Fog-img.png?alt=media"
+                  tw="w-[200px] h-[200px] rounded-full"
+                  alt="avatar"
+                  src="https://firebasestorage.googleapis.com/v0/b/chia1104.appspot.com/o/images%2Fme%2Fme.JPG?alt=media"
                 />
-                <div tw="flex absolute w-full justify-center items-center p-20">
-                  <div tw="flex flex-col justify-center items-center mr-10">
-                    <img
-                      style={{
-                        objectFit: "contain",
-                      }}
-                      tw="w-[200px] h-[200px] rounded-full"
-                      alt="avatar"
-                      src="https://firebasestorage.googleapis.com/v0/b/chia1104.appspot.com/o/images%2Fme%2Fme.JPG?alt=media"
-                    />
-                    <h1 tw="text-3xl text-slate-900">Chia1104 - 俞又嘉</h1>
-                    <h2 tw="text-xl text-slate-700">https://chia1104.dev</h2>
-                  </div>
-                  <h3 tw="text-[55px] text-slate-600 ml-10">{title}</h3>
-                </div>
+                <h1 tw="text-3xl text-slate-900">Chia1104 - 俞又嘉</h1>
+                <h2 tw="text-xl text-slate-700">https://chia1104.dev</h2>
               </div>
+              <h3 tw="text-[55px] text-slate-600 ml-10">{title}</h3>
             </div>
-          ),
-          {
-            width: 1200,
-            height: 630,
-          }
-        );
-      } catch (e: any) {
-        console.log(`${e.message}`);
-        return new Response(`Failed to generate the image`, {
-          status: 500,
-        });
+          </div>
+        </div>
+      ),
+      {
+        width: 1200,
+        height: 630,
       }
-    default:
-      return new Response(`Method ${req.method} Not Allowed`, {
-        status: 405,
-      });
+    );
+  } catch (e: any) {
+    console.log(`${e.message}`);
+    return new Response(`Failed to generate the image`, {
+      status: 500,
+    });
   }
 }
