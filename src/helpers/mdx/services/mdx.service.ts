00b8295075dcb6cfbbf2552936c9bc5eda9a5158
Chia1104 <yuyuchia7423@gmail.com>
2023-02-27 04:40:09 +0800

    :sparkles: Using `compileMDX`.

--- src/helpers/mdx/services/mdx.service.ts
+++ src/helpers/mdx/services/mdx.service.ts
@@ -3,6 +3,7 @@ import path from "path";
 import type { PostFrontMatter, PostSource } from "@chia/shared/types";
 import { POSTS_PATH } from "@chia/shared/constants";
 import { serialize } from "next-mdx-remote/serialize";
+import { compileMDX } from "next-mdx-remote/rsc";
 import pMap from "p-map";
 import glob from "fast-glob";
 import { getPostData } from "../repositories";
@@ -13,6 +14,7 @@ import rehypeHighlight from "rehype-highlight";
 import rehypeCodeTitles from "rehype-code-titles";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import { cache } from "react";
+import { Components } from "@chia/components/server/MDXRemote";
 
 const PostsPath = path.join(process.cwd(), POSTS_PATH);
 
@@ -20,17 +22,15 @@ export const preload = (slug: string) => {
   void getPost(slug);
 };
 
-export const getSlugs = async (): Promise<string[]> => {
-  const mdxFiles = await glob("*.mdx", { cwd: PostsPath });
+export const getSlugs = async (): Promise<string[]> =>
+  (await glob("*.mdx", { cwd: PostsPath })).map((fileName) =>
+    fileName.replace(/\.mdx$/, "")
+  );
 
-  return mdxFiles.map((fileName) => fileName.replace(/\.mdx$/, ""));
-};
-
-export const getEncodedSlugs = async (): Promise<string[]> => {
-  const mdxFiles = await glob("*.mdx", { cwd: PostsPath });
-
-  return mdxFiles.map((fileName) => encodeURI(fileName.replace(/\.mdx$/, "")));
-};
+export const getEncodedSlugs = async (): Promise<string[]> =>
+  (await glob("*.mdx", { cwd: PostsPath })).map((fileName) =>
+    encodeURI(fileName.replace(/\.mdx$/, ""))
+  );
 
 export const getPost = cache(async (slug: string): Promise<PostSource> => {
   const { frontMatter, content } = await getPostData(slug);
@@ -63,6 +63,46 @@ export const getPost = cache(async (slug: string): Promise<PostSource> => {
   };
 });
 
+export const getCompiledSource = cache(
+  async (
+    slug: string
+  ): Promise<{
+    content: JSX.Element;
+    frontmatter: PostFrontMatter;
+  }> => {
+    const { frontMatter, content } = await getPostData(slug);
+
+    const source = await compileMDX({
+      source: content,
+      options: {
+        mdxOptions: {
+          remarkPlugins: [[remarkGfm, { singleTilde: false }]],
+          rehypePlugins: [
+            [rehypeSlug],
+            [rehypePrism, { ignoreMissing: true }],
+            [
+              rehypeAutolinkHeadings,
+              {
+                properties: { className: ["anchor"] },
+              },
+              { behaviour: "wrap" },
+            ],
+            rehypeHighlight,
+            rehypeCodeTitles,
+          ],
+        },
+        parseFrontmatter: false,
+      },
+      compiledSource: (await serialize(content)).compiledSource,
+      components: { ...(Components as any) },
+    });
+    return {
+      frontmatter: frontMatter,
+      content: source.content,
+    };
+  }
+);
+
 export const getSource = cache(async (slug: string): Promise<string> => {
   const { content } = await getPostData(slug);
 
@@ -72,15 +112,15 @@ export const getSource = cache(async (slug: string): Promise<string> => {
 export const getAllPosts = cache(async (): Promise<PostFrontMatter[]> => {
   const slugs = await getSlugs();
 
-  const data = await pMap(
+  return await pMap(
     slugs,
     async (slug) => (await getPostData(slug)).frontMatter,
     {
       stopOnError: true,
     }
+  ).then((posts) =>
+    posts
+      .sort((a, b) => (a.createdAt > b.createdAt ? -1 : 1))
+      .filter((post) => post.published)
   );
-
-  data.sort((a, b) => (a.createdAt > b.createdAt ? -1 : 1));
-
-  return data.filter((post) => post.published);
 });
