7e77af7a93931389e2059f7cb3c4f7515a8affbc
Chia1104 <yuyuchia7423@gmail.com>
2023-03-07 13:08:46 +0800

    :art: pre-commit ???

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import {
 import { type ReactNode } from "react";
 import { Chia } from "@chia/shared/meta/chia";
 import "@total-typescript/ts-reset";
-import type { Metadata } from 'next';
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: {
