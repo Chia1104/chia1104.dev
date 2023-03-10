b6a73028d065b3d3ec44cba704abe18c7b3ae730
Chia1104 <yuyuchia7423@gmail.com>
2023-03-10 17:04:55 +0800

    :test_tube: Test.

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,14 +1,15 @@
 import { ActionIcon, Page } from "@chia/ui";
+import { JotaiProvider } from "@chia/ui/Provider";
 import { NavMenu } from "./components";
 import type { ReactNode } from "react";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <>
+    <JotaiProvider>
       <NavMenu />
       <ActionIcon />
       <Page>{children}</Page>
-    </>
+    </JotaiProvider>
   );
 };
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import { ErrorBoundary } from "@chia/ui";
-import { RootProvider, JotaiProvider } from "@chia/ui/Provider";
+import { RootProvider } from "@chia/ui/Provider";
 import { Analytics } from "@chia/app/components";
 import { type ReactNode } from "react";
 import { Chia } from "@chia/shared/meta/chia";
@@ -84,9 +84,7 @@ const ChiaWEB = ({ children }: { children: ReactNode }) => {
     <html lang="zh-Hant-TW">
       <body className="c-bg-primary scrollbar-thin scrollbar-thumb-secondary scrollbar-thumb-rounded-full">
         <ErrorBoundary>
-          <RootProvider>
-            <JotaiProvider>{children}</JotaiProvider>
-          </RootProvider>
+          <RootProvider>{children}</RootProvider>
         </ErrorBoundary>
         <Analytics />
       </body>
