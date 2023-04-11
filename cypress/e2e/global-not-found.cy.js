58a73459c21d8b14b9c681a41009837813c543ff
Chia1104 <yuyuchia7423@gmail.com>
2023-04-12 01:15:55 +0800

    * ✅ test(global-not-found.cy.js): add test to check if h1 tag contains 'Chia1104' after clicking on home page link

--- cypress/e2e/global-not-found.cy.js
+++ cypress/e2e/global-not-found.cy.js
@@ -3,10 +3,11 @@ describe("Global 404", () => {
     cy.visit("http://localhost:3000/404");
     cy.get("h1").should("contain", "404");
   });
-  it("Should go back to home page", () => {
+  it("Should go back to home page, and return Chia1104 in h1 tag", () => {
     cy.visit("http://localhost:3000/404");
     cy.get("h1").should("contain", "404");
     cy.get("a").click();
     cy.url().should("eq", "http://localhost:3000/");
+    cy.get("h1").should("contain", "Chia1104");
   });
 });
