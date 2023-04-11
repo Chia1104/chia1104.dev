69e0e22cc22443aaa5f37dd7780451bb51749b0d
Chia1104 <yuyuchia7423@gmail.com>
2023-04-12 01:19:36 +0800

    * 🐛 fix(global-not-found.cy.js): change h1 tag to h2 tag in both tests

--- cypress/e2e/global-not-found.cy.js
+++ cypress/e2e/global-not-found.cy.js
@@ -1,11 +1,11 @@
 describe("Global 404", () => {
   it("Should return 404 page", () => {
     cy.visit("http://localhost:3000/404");
-    cy.get("h1").should("contain", "404");
+    cy.get("h2").should("contain", "404");
   });
   it("Should go back to home page, and return Chia1104 in h1 tag", () => {
     cy.visit("http://localhost:3000/404");
-    cy.get("h1").should("contain", "404");
+    cy.get("h2").should("contain", "404");
     cy.get("a").click();
     cy.url().should("eq", "http://localhost:3000/");
     cy.get("h1").should("contain", "Chia1104");
