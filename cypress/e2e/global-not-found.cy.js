4474d697f5ebac8e5e26a24ca08660c20c0c5467
Chia1104 <yuyuchia7423@gmail.com>
2023-04-12 01:28:49 +0800

    * 🐛 fix(.gitignore): add cypress/screenshots to ignore list * 🚨 chore(global-not-found.cy.js): add failOnStatusCode: false to cy.visit calls

--- cypress/e2e/global-not-found.cy.js
+++ cypress/e2e/global-not-found.cy.js
@@ -1,10 +1,10 @@
 describe("Global 404", () => {
   it("Should return 404 page", () => {
-    cy.visit("http://localhost:3000/404");
+    cy.visit("http://localhost:3000/404", { failOnStatusCode: false });
     cy.get("h2").should("contain", "404");
   });
   it("Should go back to home page, and return Chia1104 in h1 tag", () => {
-    cy.visit("http://localhost:3000/404");
+    cy.visit("http://localhost:3000/404", { failOnStatusCode: false });
     cy.get("h2").should("contain", "404");
     cy.get("a").click();
     cy.url().should("eq", "http://localhost:3000/");
