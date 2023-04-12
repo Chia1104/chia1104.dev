3d0d870675899637b39e0d6304dc3e692b597ae4
Chia1104 <yuyuchia7423@gmail.com>
2023-04-12 22:46:42 +0800

    * ⚙️ chore(cypress.config.js): add defaultCommandTimeout to e2e configuration

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from "cypress";
 
+/** @type {import("cypress").CypressConfiguration} */
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
+    defaultCommandTimeout: 10000,
   },
 });
