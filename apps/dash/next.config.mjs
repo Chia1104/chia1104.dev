83119f906fbdd14311f63d07588eaf894571319f
Chia1104 <yuyuchia7423@gmail.com>
2023-04-21 22:28:25 +0800

    feat(dash): init dash

---
+++ apps/dash/next.config.mjs
@@ -0,0 +1,54 @@
+// @ts-check
+
+const securityHeaders = [
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+  {
+    key: "Referrer-Policy",
+    value: "origin-when-cross-origin",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+];
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "standalone",
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    appDir: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: securityHeaders,
+      },
+    ];
+  },
+};
+
+const plugins = [];
+
+const nextComposePlugins = plugins.reduce(
+  (acc, plugin) => plugin(acc),
+  nextConfig
+);
+
+export default nextComposePlugins;
