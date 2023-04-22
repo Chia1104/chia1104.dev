8fc7691fd5bf58d1427511d23bdfe75ea384bd48
Chia1104 <yuyuchia7423@gmail.com>
2023-04-22 19:39:25 +0800

    style(www): add `ToggleTheme` button

--- apps/www/src/app/(main)/components/NavMenu.tsx
+++ apps/www/src/app/(main)/components/NavMenu.tsx
@@ -2,10 +2,10 @@
 
 import { type FC, memo } from "react";
 import Link from "next/link";
-import { cn } from "ui";
+import { cn, ToggleTheme } from "ui";
 import { usePathname } from "next/navigation";
 import { LayoutGroup, motion } from "framer-motion";
-import { useDarkMode, useIsMounted } from "@/hooks";
+import { useDarkMode } from "@/hooks";
 
 const navItems = {
   "/about": {
@@ -84,43 +84,7 @@ const navItems = {
 
 const Toggle: FC = () => {
   const { isDarkMode, toggle } = useDarkMode();
-  const isMounted = useIsMounted();
-  return (
-    <button
-      aria-label="Light or Dark"
-      onClick={toggle}
-      className="hover:text-secondary mr-3 transition ease-in-out">
-      {isMounted && isDarkMode ? (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          strokeWidth={2}>
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-          />
-        </svg>
-      ) : (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          strokeWidth={2}>
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-          />
-        </svg>
-      )}
-    </button>
-  );
+  return <ToggleTheme toggleTheme={toggle} isDark={isDarkMode} />;
 };
 
 const NavMenu: FC = () => {
