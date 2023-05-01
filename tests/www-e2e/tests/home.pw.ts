0e3f8f621a0381dc218144f141091d28448f3735
Chia1104 <yuyuchia7423@gmail.com>
2023-05-01 21:16:59 +0800

    fix(ci): fix www e2e tests

--- tests/www-e2e/tests/home.pw.ts
+++ tests/www-e2e/tests/home.pw.ts
@@ -1,7 +1,9 @@
 import { test, expect } from "@playwright/test";
 
 test("home page", async ({ page }) => {
-  await page.goto("http://localhost:3000/");
+  await page.goto("http://127.0.0.1:3002/");
   const title = page.locator("h1");
   await expect(title).toHaveText("Chia1104 俞又嘉");
+  const img = page.locator("img");
+  await expect(img).toBeVisible();
 });
