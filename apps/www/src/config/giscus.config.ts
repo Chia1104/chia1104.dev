84c874071572658a74cf6cdc8a3691ca5752c4ce
Chia1104 <yuyuchia7423@gmail.com>
2023-05-05 21:02:20 +0800

    feat(www): add file-based metadata api

--- apps/www/src/config/giscus.config.ts
+++
@@ -1,7 +0,0 @@
-export const giscusConfig = {
-  repo: "Chia1104/chias-web-nextjs",
-  repoId: "R_kgDOHO1dfw",
-  category: "Comments",
-  categoryId: "DIC_kwDOHO1df84CPGe6",
-  theme: "dark_dimmed",
-};
