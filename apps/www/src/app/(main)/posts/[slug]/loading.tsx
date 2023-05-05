c7f2cdd9229e0e1b715ea2d83db48ee2ba040c02
Chia1104 <yuyuchia7423@gmail.com>
2023-05-05 17:45:34 +0800

    fix(www): remove cn

--- apps/www/src/app/(main)/posts/[slug]/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function PostLoading() {
-  return (
-    <div className="flex h-screen items-center justify-center">
-      <div className="loader mb-4 h-12 w-12 rounded-full border-4 border-t-4 border-gray-200 ease-linear" />
-    </div>
-  );
-}
