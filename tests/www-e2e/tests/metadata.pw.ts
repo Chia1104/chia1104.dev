9728d790fafc12b756e95a36cf8eee21120c6359
Chia1104 <yuyuchia7423@gmail.com>
2023-05-07 16:22:08 +0800

    * test(metadata.pw.ts): add tests for home og image, sitemap, and robots.txt

--- tests/www-e2e/tests/metadata.pw.ts
+++ tests/www-e2e/tests/metadata.pw.ts
@@ -4,3 +4,22 @@ test("metadata favicon.ico", async ({ page }) => {
   const response = await page.request.get("http://127.0.0.1:3002/favicon.ico");
   await expect(response).toBeOK();
 });
+
+test("test home og image", async ({ page }) => {
+  const response = await page.request.get(
+    "http://127.0.0.1:3002/opengraph-image"
+  );
+  await expect(response).toBeOK();
+});
+
+test("test sitemap", async ({ page }) => {
+  const response = await page.request.get(
+    "http://127.0.0.1:3002/sitemap-0.xml"
+  );
+  await expect(response).toBeOK();
+});
+
+test("test robots.txt", async ({ page }) => {
+  const response = await page.request.get("http://127.0.0.1:3002/robots.txt");
+  await expect(response).toBeOK();
+});
