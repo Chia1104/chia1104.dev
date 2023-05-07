4487bce89524ddb2ce40a077b0ec96443c2be2da
Chia1104 <yuyuchia7423@gmail.com>
2023-05-07 17:25:34 +0800

    last commit...

--- tests/www-e2e/tests/metadata.pw.ts
+++ tests/www-e2e/tests/metadata.pw.ts
@@ -12,12 +12,12 @@ test("test home og image", async ({ page }) => {
   await expect(response).toBeOK();
 });
 
-test("test sitemap", async ({ page }) => {
-  const response = await page.request.get(
-    "http://127.0.0.1:3002/sitemap-0.xml"
-  );
-  await expect(response).toBeOK();
-});
+// test("test sitemap", async ({ page }) => {
+//   const response = await page.request.get(
+//     "http://127.0.0.1:3002/sitemap-0.xml"
+//   );
+//   await expect(response).toBeOK();
+// });
 
 test("test robots.txt", async ({ page }) => {
   const response = await page.request.get("http://127.0.0.1:3002/robots.txt");
