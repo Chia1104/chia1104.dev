929d9de5ca12f92d6863f90f7fb41a0bf5e0cc23
Chia1104 <yuyuchia7423@gmail.com>
2023-05-10 00:51:52 +0800

    fix(backend): import PrismaClient from @prisma/client instead of db

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,8 +1,5 @@
 import { Module } from "@nestjs/common";
-import { GraphQLModule } from "@nestjs/graphql";
-import { ApolloDriver, type ApolloDriverConfig } from "@nestjs/apollo";
 import { ConfigModule } from "@nestjs/config";
-import { join } from "path";
 import { AppController } from "./app.controller";
 import PostModule from "@/modules/post/post.module";
 
@@ -10,14 +7,14 @@ import PostModule from "@/modules/post/post.module";
   imports: [
     PostModule,
     ConfigModule.forRoot(),
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), "schema.gql"),
-      buildSchemaOptions: { dateScalarMode: "timestamp" },
-      playground: true,
-      introspection: process.env.NODE_ENV !== "production",
-      persistedQueries: false,
-    }),
+    // GraphQLModule.forRoot<ApolloDriverConfig>({
+    //   driver: ApolloDriver,
+    //   autoSchemaFile: join(process.cwd(), "schema.gql"),
+    //   buildSchemaOptions: { dateScalarMode: "timestamp" },
+    //   playground: true,
+    //   introspection: process.env.NODE_ENV !== "production",
+    //   persistedQueries: false,
+    // }),
   ],
   controllers: [AppController],
 })
--- apps/backend/src/modules/prisma/prisma.service.ts
+++ apps/backend/src/modules/prisma/prisma.service.ts
@@ -1,5 +1,5 @@
 import { INestApplication, Injectable, OnModuleInit } from "@nestjs/common";
-import { PrismaClient } from "db";
+import { PrismaClient } from "@prisma/client";
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
