675ea4e5ce283e272ef3ac51baf5013cfd59e333
Chia1104 <yuyuchia7423@gmail.com>
2023-07-16 03:19:27 +0800

    fix: bump prisma to v5

--- apps/backend/src/modules/prisma/prisma.service.ts
+++ apps/backend/src/modules/prisma/prisma.service.ts
@@ -1,5 +1,6 @@
 import { INestApplication, Injectable, OnModuleInit } from "@nestjs/common";
 import { PrismaClient } from "@prisma/client";
+import * as process from "process";
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
@@ -7,9 +8,25 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
     await this.$connect();
   }
 
+  private async exitHandler(app: INestApplication) {
+    await app.close();
+  }
+
   async enableShutdownHooks(app: INestApplication) {
-    this.$on("beforeExit", async () => {
-      await app.close();
+    process.on("exit", async () => {
+      await this.exitHandler(app);
+    });
+    process.on("beforeExit", async () => {
+      await this.exitHandler(app);
+    });
+    process.on("SIGINT", async () => {
+      await this.exitHandler(app);
+    });
+    process.on("SIGTERM", async () => {
+      await this.exitHandler(app);
+    });
+    process.on("SIGUSR2", async () => {
+      await this.exitHandler(app);
     });
   }
 }
