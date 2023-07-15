4f1a2843d7b87269b0c6b3137b8f11ace2e12666
Chia1104 <yuyuchia7423@gmail.com>
2023-07-16 04:55:19 +0800

    feat: change to swc and update prisma service

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -4,6 +4,7 @@ import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableShutdownHooks();
   const config = new DocumentBuilder()
     .setTitle("NestJS Example")
     .setVersion("1.0")
