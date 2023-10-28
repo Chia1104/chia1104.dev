16b8dd2b802cd0a6f893bf98565788bbbb0ee8d3
Chia1104 <yuyuchia7423@gmail.com>
2023-10-29 00:29:46 +0800

    fix: fix some type issues

--- config/tailwind-config/index.d.ts
+++ config/tailwind-config/index.d.ts
@@ -1,10 +1,10 @@
 import type { Config } from "tailwindcss";
 
-declare const baseConfig: Config | undefined;
+declare const baseConfig: Partial<Config>;
 
-declare const animation:
-  | (Config["theme"]["animation"] & Config["theme"]["keyframes"])
-  | undefined;
+declare const animation: Partial<
+  Config["theme"]["animation"] & Config["theme"]["keyframes"]
+>;
 
 export default baseConfig;
 export { animation };
