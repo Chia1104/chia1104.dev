1e46cba5cbea890b2b908870b9b3095be2ed65cb
Chia1104 <yuyuchia7423@gmail.com>
2023-11-17 16:48:05 +0800

    fix(www): fix the animation on mobile

--- apps/www/src/app/(main)/projects/page.tsx
+++ apps/www/src/app/(main)/projects/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { Image, Card } from "@chia/ui";
+import { Image, Card, FadeIn } from "@chia/ui";
 import { getPinnedRepos } from "@/helpers/GraphQL/github";
 import meta from "@chia/meta";
 import { type FC } from "react";
@@ -19,54 +19,44 @@ const RepoCard: FC<{
 }> = ({ image, name, description, tags, language, updatedAt, href }) => {
   const updatedAtText = dayjs(updatedAt).format("MMMM D, YYYY");
   return (
-    <Card
-      className="relative flex h-full min-h-[442px] flex-col"
-      whileInView={{
-        opacity: 1,
-        y: 0,
-        transition: {
-          duration: 0.5,
-        },
-      }}
-      initial={{
-        opacity: 0,
-        y: 20,
-      }}>
-      <div className="aspect-h-9 aspect-w-16 c-bg-gradient-green-to-purple not-prose w-full overflow-hidden rounded-t-3xl">
-        <Image
-          src={image}
-          alt={name}
-          className="object-cover"
-          loading="lazy"
-          fill
-          sizes="100vw"
-        />
-      </div>
-      <div className="flex h-full flex-col p-4 pt-0">
-        <h2 className="mt-5 text-2xl font-bold">{name}</h2>
-        <p className="text-sm text-gray-500">{updatedAtText}</p>
-        <p className="mt-2 line-clamp-2 text-base">{description}</p>
-        <div className="mt-2 flex flex-wrap space-x-2">
-          {tags?.map((tag) => (
-            <span
-              key={tag}
-              className="rounded bg-gray-800 px-2 py-1 text-sm font-medium text-white">
-              {tag}
-            </span>
-          ))}
+    <FadeIn className="w-full overflow-hidden rounded-3xl">
+      <Card className="relative flex h-full min-h-[442px] flex-col">
+        <div className="aspect-h-9 aspect-w-16 c-bg-gradient-green-to-purple not-prose w-full overflow-hidden rounded-t-3xl">
+          <Image
+            src={image}
+            alt={name}
+            className="object-cover"
+            loading="lazy"
+            fill
+            sizes="100vw"
+          />
         </div>
-        <div className="mt-auto flex flex-wrap space-x-2">
-          {language && (
-            <span
-              className="rounded bg-gray-800 px-2 py-1 text-sm font-medium text-white"
-              style={{ backgroundColor: language.color }}>
-              {language.name}
-            </span>
-          )}
+        <div className="flex h-full flex-col p-4 pt-0">
+          <h2 className="mt-5 text-2xl font-bold">{name}</h2>
+          <p className="text-sm text-gray-500">{updatedAtText}</p>
+          <p className="mt-2 line-clamp-2 text-base">{description}</p>
+          <div className="mt-2 flex flex-wrap space-x-2">
+            {tags?.map((tag) => (
+              <span
+                key={tag}
+                className="rounded bg-gray-800 px-2 py-1 text-sm font-medium text-white">
+                {tag}
+              </span>
+            ))}
+          </div>
+          <div className="mt-auto flex flex-wrap space-x-2">
+            {language && (
+              <span
+                className="rounded bg-gray-800 px-2 py-1 text-sm font-medium text-white"
+                style={{ backgroundColor: language.color }}>
+                {language.name}
+              </span>
+            )}
+          </div>
         </div>
-      </div>
-      <Link href={href} className="absolute inset-0 z-10" />
-    </Card>
+        <Link href={href} className="absolute inset-0 z-10" />
+      </Card>
+    </FadeIn>
   );
 };
 
