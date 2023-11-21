2908e1c1e0eac3cf20b7ec0fab0ebcb421b047d1
Chia1104 <yuyuchia7423@gmail.com>
2023-11-21 19:03:06 +0800

    fix(utils): fix crypto api

--- packages/utils/src/generate-slug/generate-slug.util.spec.ts
+++ packages/utils/src/generate-slug/generate-slug.util.spec.ts
@@ -2,7 +2,7 @@ import generateSlug from "./generate-slug.util";
 import { describe, it } from "vitest";
 
 describe("generateSlug", () => {
-  it("should return a slug", () => {
+  it("should return a slug", async () => {
     const slug = generateSlug("Hello World");
     console.log(slug);
   });
--- packages/utils/src/generate-slug/generate-slug.util.ts
+++ packages/utils/src/generate-slug/generate-slug.util.ts
@@ -1,18 +1,16 @@
 import { getToday } from "../day/day.util";
-import crypto from "node:crypto";
 
 const generateSlug = (title: string) => {
-  return getToday() +
+  return (
+    getToday() +
     "-" +
     title
       .toLowerCase()
       .replace(/[^\w ]+/g, "")
       .replace(/ +/g, "-") +
     "-" +
-    typeof crypto.randomBytes ===
-    "function"
-    ? crypto.randomBytes(4).toString("hex")
-    : crypto.randomUUID();
+    crypto.randomUUID()
+  );
 };
 
 export default generateSlug;
