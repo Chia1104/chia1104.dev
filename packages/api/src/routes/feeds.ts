19fc5f3ca7a3de31d0a69c671815e699b070977c
Chia1104 <yuyuchia7423@gmail.com>
2023-11-30 12:49:09 +0800

    fix(db): fix multiple instance issue

--- packages/api/src/routes/feeds.ts
+++ packages/api/src/routes/feeds.ts
@@ -1,123 +1,43 @@
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure, adminProcedure } from "../trpc";
-import { FeedsAPI, eq, schema } from "@chia/db";
+import {
+  eq,
+  schema,
+  getFeeds,
+  getInfiniteFeeds,
+  getFeedsByUserId,
+  getInfiniteFeedsByUserId,
+} from "@chia/db";
+import { getSchema, infiniteSchema } from "@chia/db/src/utils/validator/feeds";
 
 export const feedsRouter = createTRPCRouter({
-  get: protectedProcedure
-    .input(
-      z
-        .object({
-          take: z.number().max(50).optional().default(10),
-          skip: z.number().optional().default(0),
-          orderBy: z
-            .enum(["createdAt", "updatedAt", "id", "slug", "title"])
-            .optional()
-            .default("updatedAt"),
-          sortOrder: z.enum(["asc", "desc"]).optional().default("desc"),
-          type: z.enum(["post", "note"]).optional().default("post"),
-        })
-        .optional()
-        .default({
-          take: 10,
-          skip: 0,
-          orderBy: "updatedAt",
-          sortOrder: "desc",
-          type: "post",
-        })
-    )
-    .query(async (opts) => {
-      return new FeedsAPI(opts.ctx.db).getFeeds({
-        ...opts.input,
-        whereAnd: [eq(schema.feeds.userId, opts.ctx.session.user.id)],
-      });
-    }),
+  get: protectedProcedure.input(getSchema).query(async (opts) => {
+    return getFeeds(opts.ctx.db, {
+      ...opts.input,
+      whereAnd: [eq(schema.feeds.userId, opts.ctx.session.user.id)],
+    });
+  }),
 
-  infinite: protectedProcedure
-    .input(
-      z
-        .object({
-          limit: z.number().max(50).optional().default(10),
-          cursor: z.any(),
-          orderBy: z
-            .enum(["createdAt", "updatedAt", "id", "slug", "title"])
-            .optional()
-            .default("updatedAt"),
-          sortOrder: z.enum(["asc", "desc"]).optional().default("desc"),
-          type: z.enum(["post", "note"]).optional().default("post"),
-        })
-        .optional()
-        .default({
-          limit: 10,
-          cursor: null,
-          orderBy: "updatedAt",
-          sortOrder: "desc",
-          type: "post",
-        })
-    )
-    .query(async (opts) => {
-      return new FeedsAPI(opts.ctx.db).getInfiniteFeeds({
-        ...opts.input,
-        whereAnd: [eq(schema.feeds.userId, opts.ctx.session.user.id)],
-      });
-    }),
+  infinite: protectedProcedure.input(infiniteSchema).query(async (opts) => {
+    return getInfiniteFeeds(opts.ctx.db, {
+      ...opts.input,
+      whereAnd: [eq(schema.feeds.userId, opts.ctx.session.user.id)],
+    });
+  }),
 
-  getByAdmin: adminProcedure
-    .input(
-      z
-        .object({
-          take: z.number().optional().default(10),
-          skip: z.number().optional().default(0),
-          orderBy: z
-            .enum(["createdAt", "updatedAt", "id", "slug", "title"])
-            .optional()
-            .default("updatedAt"),
-          sortOrder: z.enum(["asc", "desc"]).optional().default("desc"),
-          type: z.enum(["post", "note"]).optional().default("post"),
-        })
-        .optional()
-        .default({
-          take: 10,
-          skip: 0,
-          orderBy: "updatedAt",
-          sortOrder: "desc",
-          type: "post",
-        })
-    )
-    .query(async (opts) => {
-      return new FeedsAPI(opts.ctx.db).getFeedsByUserId({
-        ...opts.input,
-        userId: opts.ctx.adminId,
-        whereAnd: [eq(schema.feeds.published, true)],
-      });
-    }),
+  getByAdmin: adminProcedure.input(getSchema).query(async (opts) => {
+    return getFeedsByUserId(opts.ctx.db, {
+      ...opts.input,
+      userId: opts.ctx.adminId,
+      whereAnd: [eq(schema.feeds.published, true)],
+    });
+  }),
 
-  infinityByAdmin: adminProcedure
-    .input(
-      z
-        .object({
-          limit: z.number().optional().default(10),
-          cursor: z.any(),
-          orderBy: z
-            .enum(["createdAt", "updatedAt", "id", "slug", "title"])
-            .optional()
-            .default("updatedAt"),
-          sortOrder: z.enum(["asc", "desc"]).optional().default("desc"),
-          type: z.enum(["post", "note"]).optional().default("post"),
-        })
-        .optional()
-        .default({
-          limit: 10,
-          cursor: null,
-          orderBy: "updatedAt",
-          sortOrder: "desc",
-          type: "post",
-        })
-    )
-    .query(async (opts) => {
-      return new FeedsAPI(opts.ctx.db).getInfiniteFeedsByUserId({
-        ...opts.input,
-        userId: opts.ctx.adminId,
-        whereAnd: [eq(schema.feeds.published, true)],
-      });
-    }),
+  infinityByAdmin: adminProcedure.input(infiniteSchema).query(async (opts) => {
+    return getInfiniteFeedsByUserId(opts.ctx.db, {
+      ...opts.input,
+      userId: opts.ctx.adminId,
+      whereAnd: [eq(schema.feeds.published, true)],
+    });
+  }),
 });
--- packages/db/src/utils/feeds/index.ts
+++ packages/db/src/utils/feeds/index.ts
@@ -1,12 +1,23 @@
 import { type DB } from "../..";
 import dayjs from "dayjs";
 import { type SQLWrapper } from "drizzle-orm";
+import type { GetDTO, InfiniteDTO } from "../validator/feeds";
 
+let instance: FeedsAPI | undefined;
+
+/**
+ * @deprecated
+ */
 export class FeedsAPI {
   private db: DB;
 
   constructor(db: DB) {
+    if (instance) {
+      throw new Error("You can only create one instance!");
+    }
     this.db = db;
+    // eslint-disable-next-line @typescript-eslint/no-this-alias
+    instance = this;
   }
 
   async getFeeds({
@@ -16,12 +27,7 @@ export class FeedsAPI {
     sortOrder = "desc",
     type = "post",
     whereAnd = [],
-  }: {
-    take?: number;
-    skip?: number;
-    orderBy?: "createdAt" | "updatedAt" | "id" | "slug" | "title";
-    sortOrder?: "asc" | "desc";
-    type?: "post" | "note";
+  }: GetDTO & {
     whereAnd?: (SQLWrapper | undefined)[];
   }) {
     const feedType =
@@ -51,12 +57,7 @@ export class FeedsAPI {
     type = "post",
     userId,
     whereAnd = [],
-  }: {
-    take?: number;
-    skip?: number;
-    orderBy?: "createdAt" | "updatedAt" | "id" | "slug" | "title";
-    sortOrder?: "asc" | "desc";
-    type?: "post" | "note";
+  }: GetDTO & {
     userId: string;
     whereAnd?: (SQLWrapper | undefined)[];
   }) {
@@ -87,12 +88,7 @@ export class FeedsAPI {
     sortOrder = "desc",
     type = "post",
     whereAnd = [],
-  }: {
-    limit: number;
-    cursor?: any;
-    orderBy: "createdAt" | "updatedAt" | "id" | "slug" | "title";
-    sortOrder: "asc" | "desc";
-    type?: "post" | "note";
+  }: InfiniteDTO & {
     whereAnd?: (SQLWrapper | undefined)[];
   }) {
     const cursorTransform = (cursor: any) => {
@@ -145,12 +141,7 @@ export class FeedsAPI {
     type = "post",
     userId,
     whereAnd = [],
-  }: {
-    limit: number;
-    cursor?: any;
-    orderBy: "createdAt" | "updatedAt" | "id" | "slug" | "title";
-    sortOrder: "asc" | "desc";
-    type?: "post" | "note";
+  }: InfiniteDTO & {
     userId: string;
     whereAnd?: (SQLWrapper | undefined)[];
   }) {
@@ -198,3 +189,184 @@ export class FeedsAPI {
     };
   }
 }
+
+export const getFeeds = async (
+  db: DB,
+  {
+    take = 10,
+    skip = 0,
+    orderBy = "updatedAt",
+    sortOrder = "desc",
+    type = "post",
+    whereAnd = [],
+  }: GetDTO & {
+    whereAnd?: (SQLWrapper | undefined)[];
+  }
+) => {
+  const feedType =
+    type === "post"
+      ? ({
+          post: true,
+        } as const)
+      : ({
+          note: true,
+        } as const);
+  return db.query.feeds.findMany({
+    orderBy: (feeds, { asc, desc }) => [
+      sortOrder === "asc" ? asc(feeds[orderBy]) : desc(feeds[orderBy]),
+    ],
+    limit: take,
+    offset: skip,
+    where: (feeds, { eq, and }) => and(eq(feeds.type, type), ...whereAnd),
+    with: feedType,
+  });
+};
+
+export const getFeedsByUserId = async (
+  db: DB,
+  {
+    take = 10,
+    skip = 0,
+    orderBy = "updatedAt",
+    sortOrder = "desc",
+    type = "post",
+    userId,
+    whereAnd = [],
+  }: GetDTO & {
+    userId: string;
+    whereAnd?: (SQLWrapper | undefined)[];
+  }
+) => {
+  const feedType =
+    type === "post"
+      ? ({
+          post: true,
+        } as const)
+      : ({
+          note: true,
+        } as const);
+  return db.query.feeds.findMany({
+    orderBy: (feeds, { asc, desc }) => [
+      sortOrder === "asc" ? asc(feeds[orderBy]) : desc(feeds[orderBy]),
+    ],
+    limit: take,
+    offset: skip,
+    where: (feeds, { eq, and }) =>
+      and(eq(feeds.type, type), eq(feeds.userId, userId), ...whereAnd),
+    with: feedType,
+  });
+};
+
+export const getInfiniteFeeds = async (
+  db: DB,
+  {
+    limit = 10,
+    cursor,
+    orderBy = "updatedAt",
+    sortOrder = "desc",
+    type = "post",
+    whereAnd = [],
+  }: InfiniteDTO & {
+    whereAnd?: (SQLWrapper | undefined)[];
+  }
+) => {
+  const cursorTransform = (cursor: any) => {
+    if (orderBy === "createdAt" || orderBy === "updatedAt") {
+      return dayjs(cursor).toDate();
+    }
+    return cursor;
+  };
+  const feedType =
+    type === "post"
+      ? ({
+          post: true,
+        } as const)
+      : ({
+          note: true,
+        } as const);
+  const items = await db.query.feeds.findMany({
+    orderBy: (feeds, { asc, desc }) => [
+      sortOrder === "asc" ? asc(feeds[orderBy]) : desc(feeds[orderBy]),
+    ],
+    limit: limit + 1,
+    with: feedType,
+    where: cursor
+      ? (feeds, { gte, lte, eq, and }) =>
+          and(
+            sortOrder === "asc"
+              ? gte(feeds[orderBy], cursorTransform(cursor))
+              : lte(feeds[orderBy], cursorTransform(cursor)),
+            eq(feeds.type, type),
+            ...whereAnd
+          )
+      : (feeds, { eq, and }) => and(eq(feeds.type, type), ...whereAnd),
+  });
+  let nextCursor: typeof cursor | undefined = undefined;
+  if (items.length > limit) {
+    const nextItem = items.pop();
+    nextCursor = nextItem?.[orderBy];
+  }
+  return {
+    items,
+    nextCursor,
+  };
+};
+
+export const getInfiniteFeedsByUserId = async (
+  db: DB,
+  {
+    limit = 10,
+    cursor,
+    orderBy = "updatedAt",
+    sortOrder = "desc",
+    type = "post",
+    userId,
+    whereAnd = [],
+  }: InfiniteDTO & {
+    userId: string;
+    whereAnd?: (SQLWrapper | undefined)[];
+  }
+) => {
+  const cursorTransform = (cursor: any) => {
+    if (orderBy === "createdAt" || orderBy === "updatedAt") {
+      return dayjs(cursor).toDate();
+    }
+    return cursor;
+  };
+  const feedType =
+    type === "post"
+      ? ({
+          post: true,
+        } as const)
+      : ({
+          note: true,
+        } as const);
+  const items = await db.query.feeds.findMany({
+    orderBy: (feeds, { asc, desc }) => [
+      sortOrder === "asc" ? asc(feeds[orderBy]) : desc(feeds[orderBy]),
+    ],
+    limit: limit + 1,
+    with: feedType,
+    where: cursor
+      ? (feeds, { gte, lte, eq, and }) =>
+          and(
+            sortOrder === "asc"
+              ? gte(feeds[orderBy], cursorTransform(cursor))
+              : lte(feeds[orderBy], cursorTransform(cursor)),
+            eq(feeds.type, type),
+            eq(feeds.userId, userId),
+            ...whereAnd
+          )
+      : (feeds, { eq, and }) =>
+          and(eq(feeds.type, type), eq(feeds.userId, userId), ...whereAnd),
+  });
+  let nextCursor: typeof cursor | undefined = undefined;
+  if (items.length > limit) {
+    const nextItem = items.pop();
+    nextCursor = nextItem?.[orderBy];
+  }
+  return {
+    items,
+    nextCursor,
+  };
+};
