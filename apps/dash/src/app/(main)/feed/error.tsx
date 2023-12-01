2642924fd1b03d266fcdc357e3e9910b595536b1
Chia1104 <yuyuchia7423@gmail.com>
2023-12-01 18:00:41 +0800

    fix(dash): handle trpc error

--- apps/dash/src/app/(main)/feed/error.tsx
+++ apps/dash/src/app/(main)/feed/error.tsx
@@ -1,32 +1,51 @@
 "use client";
 
 import { useEffect } from "react";
+import { getHTTPStatusCodeFromError } from "@trpc/server/http";
+import { TRPCError } from "@trpc/server";
 
-export default function Error({ error }: { error?: Error }) {
+export default function Error({
+  error,
+}: {
+  error?: Error & { digest?: string };
+}) {
   useEffect(() => {
     if (error) {
       console.error(error);
     }
   }, [error]);
-  switch (error?.message) {
-    case "FORBIDDEN":
-    case "UNAUTHORIZED":
-      return (
-        <div className="c-container main">
-          <div className="text-center">
-            <h1 className="text-3xl font-bold">401</h1>
-            <p className="text-xl font-bold">You are not logged in.</p>
+  if (error instanceof TRPCError) {
+    const statusCode = getHTTPStatusCodeFromError(error);
+    switch (statusCode) {
+      case 403:
+      case 401:
+        return (
+          <div className="c-container main">
+            <div className="text-center">
+              <h1 className="text-3xl font-bold">401</h1>
+              <p className="text-xl font-bold">You are not logged in.</p>
+            </div>
           </div>
-        </div>
-      );
-    default:
-      return (
-        <div className="c-container main">
-          <div className="text-center">
-            <h1 className="text-3xl font-bold">500</h1>
-            <p className="text-xl font-bold">Something went wrong.</p>
+        );
+      default:
+        return (
+          <div className="c-container main">
+            <div className="text-center">
+              <h1 className="text-3xl font-bold">{statusCode}</h1>
+              <p className="text-xl font-bold">Something went wrong.</p>
+            </div>
           </div>
-        </div>
-      );
+        );
+    }
   }
+  return (
+    <div className="c-container main">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold">500 - {error?.name}</h1>
+        <p className="text-xl font-bold">
+          {error?.message ?? "Something went wrong."}
+        </p>
+      </div>
+    </div>
+  );
 }
