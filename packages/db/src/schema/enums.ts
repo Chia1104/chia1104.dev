d29e5d9759178aee815ece4ff209e9012aa9683f
Chia1104 <yuyuchia7423@gmail.com>
2023-12-01 19:42:01 +0800

    refactor(db): add article_type column

--- packages/db/src/schema/enums.ts
+++ packages/db/src/schema/enums.ts
@@ -3,3 +3,23 @@ import { pgEnum } from "drizzle-orm/pg-core";
 export const roles = pgEnum("role", ["admin", "user"]);
 
 export const feed_type = pgEnum("feed_type", ["post", "note"]);
+
+export const article_type = pgEnum("article_type", [
+  /**
+   * @default
+   */
+  "mdx",
+  "md",
+  /**
+   * @todo
+   */
+  "notion",
+  /**
+   * @todo
+   */
+  "sanity",
+  /**
+   * @todo
+   */
+  "tiptap",
+]);
