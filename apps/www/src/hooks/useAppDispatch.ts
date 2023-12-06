80b99633b342bc66f636d4516f5439accfaf0671
Chia1104 <yuyuchia7423@gmail.com>
2023-12-06 18:12:18 +0800

    feat(www): hello useSyncExternalStore

--- apps/www/src/hooks/useAppDispatch.ts
+++
@@ -1,4 +0,0 @@
-import { useDispatch } from "react-redux";
-import { type AppDispatch } from "@/store/type";
-
-export const useAppDispatch = () => useDispatch<AppDispatch>();
--- apps/www/src/hooks/useAppSelector.ts
+++
@@ -1,4 +0,0 @@
-import { type TypedUseSelectorHook, useSelector } from "react-redux";
-import { AppState } from "@/store/type";
-
-export const useAppSelector: TypedUseSelectorHook<AppState> = useSelector;
