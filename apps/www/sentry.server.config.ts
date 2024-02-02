71a492de4756499e8c76f1e6ee4865082c6bdf8c
Chia1104 <yuyuchia7423@gmail.com>
2024-02-02 11:21:08 +0800

    fix: enable sentry on preview

--- apps/www/sentry.server.config.ts
+++ apps/www/sentry.server.config.ts
@@ -4,6 +4,6 @@ import { env } from "./src/env.mjs";
 Sentry.init({
   dsn: env.NEXT_PUBLIC_SENTRY_DSN,
   tracesSampleRate: 1.0,
-  enabled: ["production"].includes(env.NEXT_PUBLIC_ENV),
+  enabled: ["production", "preview"].includes(env.NEXT_PUBLIC_ENV),
   environment: env.NEXT_PUBLIC_ENV,
 });
