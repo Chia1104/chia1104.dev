ae2679082614e6d73d33d8f6f2bded9af5c49dd0
Chia1104 <yuyuchia7423@gmail.com>
2024-02-16 15:09:15 +0800

    fix(auth): update env

--- packages/auth/env.ts
+++ packages/auth/env.ts
@@ -10,13 +10,14 @@ export const env = createEnv({
         ? z.string().min(1)
         : z.string().min(1).optional(),
     AUTH_REDIRECT_PROXY_URL: z.string().optional(),
+    AUTH_URL: z.string().optional(),
   },
   runtimeEnv: {
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
     AUTH_SECRET: process.env.AUTH_SECRET,
-    AUTH_REDIRECT_PROXY_URL:
-      process.env.AUTH_REDIRECT_PROXY_URL ?? process.env.AUTH_URL,
+    AUTH_REDIRECT_PROXY_URL: process.env.AUTH_REDIRECT_PROXY_URL,
+    AUTH_URL: process.env.AUTH_URL,
   },
   skipValidation:
     process.env.SKIP_ENV_VALIDATION === "true" ||
