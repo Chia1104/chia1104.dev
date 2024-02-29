1a22a43987ab5e8dc562882937dabc99623450ed
Chia1104 <yuyuchia7423@gmail.com>
2024-02-29 11:54:42 +0800

    fix(auth): update NextAuth configuration with dynamic cookie domain

--- packages/auth/index.ts
+++ packages/auth/index.ts
@@ -1,10 +1,11 @@
-import NextAuth, { type NextAuthConfig, type NextAuthResult } from "next-auth";
+import NextAuth, { type NextAuthConfig } from "next-auth";
 import { db, tableCreator, localDb, betaDb } from "@chia/db";
 import type { DefaultSession } from "@auth/core/types";
 import Google from "next-auth/providers/google";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { env } from "./env";
 import { getDb } from "@chia/utils";
+import type { NextRequest } from "next/server";
 
 declare module "@auth/core/types" {
   interface Session extends DefaultSession {
@@ -34,59 +35,72 @@ const AUTH_URL = env.AUTH_URL?.replace(/\/api\/auth$/, "");
 const useSecureCookies = process.env.NODE_ENV === "production";
 const cookiePrefix = useSecureCookies ? "__Secure-" : "";
 
-export const config = {
-  trustHost: true,
-  useSecureCookies,
-  cookies: {
-    sessionToken: {
-      name: `${cookiePrefix}authjs.session-token`,
-      options: {
-        httpOnly: true,
-        sameSite: "lax",
-        path: "/",
-        secure: useSecureCookies,
-        domain:
-          AUTH_URL?.includes("localhost") ||
-          process.env.NODE_ENV === "development"
-            ? "localhost"
-            : env.AUTH_COOKIE_DOMAIN,
+export const getCookieDomain = (req?: NextRequest) => {
+  if (
+    AUTH_URL?.includes("localhost") ||
+    process.env.NODE_ENV === "development"
+  ) {
+    return "localhost";
+  }
+  return (
+    env.AUTH_COOKIE_DOMAIN ??
+    AUTH_URL?.replace(/(^\w+:|^)\/\//, "") ??
+    req?.nextUrl.hostname ??
+    ".chia1104.dev"
+  );
+};
+
+export const getBaseConfig = (req?: NextRequest) => {
+  return {
+    trustHost: true,
+    useSecureCookies,
+    cookies: {
+      sessionToken: {
+        name: `${cookiePrefix}authjs.session-token`,
+        options: {
+          httpOnly: true,
+          sameSite: "lax",
+          path: "/",
+          secure: useSecureCookies,
+          domain: getCookieDomain(req),
+        },
       },
     },
-  },
-  pages: {
-    signIn: "/login",
-  },
-  callbacks: {
-    session: ({ session, user }) => ({
-      ...session,
-      user: {
-        ...session.user,
-        role: user.role,
-        id: user.id,
-      },
-    }),
-  },
-  adapter: DrizzleAdapter(
-    getDb(undefined, {
-      db,
-      betaDb,
-      localDb,
-    }),
-    tableCreator
-  ),
-  providers: [
-    Google({
-      clientId: env.GOOGLE_CLIENT_ID,
-      clientSecret: env.GOOGLE_CLIENT_SECRET,
-      allowDangerousEmailAccountLinking: true,
-    }),
-  ],
-} satisfies NextAuthConfig;
+    pages: {
+      signIn: "/login",
+    },
+    callbacks: {
+      session: ({ session, user }) => ({
+        ...session,
+        user: {
+          ...session.user,
+          role: user.role,
+          id: user.id,
+        },
+      }),
+    },
+    adapter: DrizzleAdapter(
+      getDb(undefined, {
+        db,
+        betaDb,
+        localDb,
+      }),
+      tableCreator
+    ),
+    providers: [
+      Google({
+        clientId: env.GOOGLE_CLIENT_ID,
+        clientSecret: env.GOOGLE_CLIENT_SECRET,
+        allowDangerousEmailAccountLinking: true,
+      }),
+    ],
+  } satisfies NextAuthConfig;
+};
 
 export const {
   handlers: { GET, POST },
   auth,
   signIn,
   signOut,
-} = NextAuth(config) as NextAuthResult;
+} = NextAuth(getBaseConfig());
 export type { Session } from "@auth/core/types";
