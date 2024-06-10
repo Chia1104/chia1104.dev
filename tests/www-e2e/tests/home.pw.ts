fb3d728f315bd58b759e506891d25c94d8d3905b
Chia1104 <yuyuchia7423@gmail.com>
2024-06-11 03:12:36 +0800

    fix(www-e2e): update test spec

--- tests/www-e2e/tests/home.pw.ts
+++ tests/www-e2e/tests/home.pw.ts
@@ -4,6 +4,4 @@ test("home page", async ({ page }) => {
   await page.goto("http://127.0.0.1:3002/");
   const title = page.locator("h1");
   await expect(title).toHaveText("Chia1104");
-  const img = page.getByAltText("Chia1104");
-  await expect(img).toBeVisible();
 });
