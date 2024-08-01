19c65a7cf9066badafec94ccb6f90e0e2f390c49
Chia1104 <yuyuchia7423@gmail.com>
2024-08-01 20:41:54 +0800

    fix: ignore some type check

--- toolings/vitest/src/react.ts
+++ toolings/vitest/src/react.ts
@@ -2,6 +2,7 @@ import react from "@vitejs/plugin-react-swc";
 import { defineProject } from "vitest/config";
 
 export default defineProject({
+  // @ts-ignore
   plugins: [react()],
   test: {
     environment: "happy-dom",
