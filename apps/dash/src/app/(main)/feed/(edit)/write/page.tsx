8990bd1d9dfe53b97f326672e4bdc5c2e1c43fc7
Chia1104 <yuyuchia7423@gmail.com>
2024-08-18 09:41:54 +0800

    feat: enable create

--- apps/dash/src/app/(main)/feed/(edit)/write/page.tsx
+++ apps/dash/src/app/(main)/feed/(edit)/write/page.tsx
@@ -2,18 +2,14 @@ import { Suspense } from "react";
 
 import dynamic from "next/dynamic";
 
-import { Features, ErrorBoundary } from "@chia/ui";
-
-import { env } from "@/env";
+import { ErrorBoundary } from "@chia/ui";
 
 const CreateForm = dynamic(() => import("./create-form"), {
   ssr: false,
 });
 
 const Page = () => {
-  return env.NODE_ENV === "production" ? (
-    <Features.ComingSoon />
-  ) : (
+  return (
     <ErrorBoundary>
       <Suspense>
         <CreateForm />
