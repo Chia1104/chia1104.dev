271a6a904b32c20185f5c8aedcb91d3b3a0c5f2f
Chia1104 <yuyuchia7423@gmail.com>
2024-08-22 00:14:34 +0800

    fix(service): check ip...

--- apps/service/src/bootstrap.ts
+++ apps/service/src/bootstrap.ts
@@ -29,77 +29,6 @@ const bootstrap = <TContext extends HonoContext>(
   app.use(logger());
 
   /**
-   * CORS middleware
-   */
-  app.use(
-    cors({
-      origin: getCORSAllowedOrigin(),
-      credentials: true,
-    })
-  );
-
-  /**
-   * Auth.js middleware
-   */
-  app.use(
-    "*",
-    initAuthConfig(() =>
-      getConfig(undefined, {
-        basePath: "/auth",
-      })
-    )
-  );
-
-  /**
-   * Sentry middleware
-   */
-  app.use(
-    "*",
-    sentry({
-      dsn: env.SENTRY_DSN,
-      enabled: env.NODE_ENV === "production" && !!env.ZEABUR_SERVICE_ID,
-    })
-  );
-
-  /**
-   * Rate limiter middleware
-   */
-  env.NODE_ENV === "production" &&
-    !!env.ZEABUR_SERVICE_ID &&
-    app.use(
-      rateLimiter({
-        windowMs: 15 * 60 * 1000, // 15 minutes
-        limit: 87, // Limit each IP to 87 requests per `window` (here, per 15 minutes).
-        standardHeaders: "draft-6", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
-        keyGenerator: (c) => {
-          let info: ReturnType<typeof getConnInfo> | null = null;
-          try {
-            info = getConnInfo(c);
-          } catch (e) {
-            console.error(e);
-            info = null;
-          }
-          return `root-request:${info?.remote.address}`;
-        },
-        // @ts-expect-error - Known issue: the `call` function is not present in @types/ioredis
-        store: new RedisStore({
-          // @ts-expect-error - Known issue: the `call` function is not present in @types/ioredis
-          sendCommand: (...args: string[]) => createRedis().call(...args),
-        }),
-      })
-    );
-
-  /**
-   * @TODO: customize schema
-   */
-  // app.use("/graphql", (c, n) =>
-  //   graphqlServer({
-  //     schema: buildSchema(c.var.db).schema,
-  //     graphiql: true,
-  //   })(c, n)
-  // );
-
-  /**
    * Global error handler
    */
   app.onError((e, c) => {
@@ -118,6 +47,69 @@ const bootstrap = <TContext extends HonoContext>(
     c.get("sentry").captureException(e);
     return c.json(errorGenerator(500), 500);
   });
+
+  /**
+   * CORS middleware
+   */
+  app.use(
+    cors({
+      origin: getCORSAllowedOrigin(),
+      credentials: true,
+    })
+  );
+
+  /**
+   * Sentry middleware
+   */
+  app.use(
+    "*",
+    sentry({
+      dsn: env.SENTRY_DSN,
+      enabled: env.NODE_ENV === "production" && !!env.ZEABUR_SERVICE_ID,
+    })
+  );
+
+  /**
+   * Auth.js middleware
+   */
+  app.use(
+    "*",
+    initAuthConfig(() =>
+      getConfig(undefined, {
+        basePath: "/auth",
+      })
+    )
+  );
+
+  /**
+   * Rate limiter middleware
+   */
+  // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+  env.NODE_ENV === "production" &&
+    !!env.ZEABUR_SERVICE_ID &&
+    app.use(
+      rateLimiter({
+        windowMs: env.RATELIMIT_WINDOW_MS,
+        limit: env.RATELIMIT_MAX,
+        standardHeaders: "draft-6", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+        keyGenerator: (c) => {
+          let info: ReturnType<typeof getConnInfo> | null = null;
+          try {
+            info = getConnInfo(c);
+          } catch (e) {
+            console.error(e);
+            info = null;
+          }
+          console.log(`root-request:${info?.remote.address}`);
+          return `root-request:${info?.remote.address}`;
+        },
+        // @ts-expect-error - Known issue: the `call` function is not present in @types/ioredis
+        store: new RedisStore({
+          // @ts-expect-error - Known issue: the `call` function is not present in @types/ioredis
+          sendCommand: (...args: string[]) => createRedis().call(...args),
+        }),
+      })
+    );
 
   /**
    * Routes
