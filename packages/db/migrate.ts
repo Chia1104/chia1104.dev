770d1ec4cbdcee79fd484849f2957b2de8a70091
Chia1104 <yuyuchia7423@gmail.com>
2024-10-16 12:05:36 +0800

    feat: update drizzle

--- packages/db/migrate.ts
+++ packages/db/migrate.ts
@@ -1,13 +1,11 @@
 import { migrate } from "drizzle-orm/postgres-js/migrator";
 
-import { getDB, closeClient } from "./src";
+import { getDB } from "./src";
 
 const run = async () => {
   await migrate(getDB(), {
     migrationsFolder: "./.drizzle/migrations",
   });
-
-  await closeClient();
 };
 
 run().catch(console.error);
--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,7 +1,6 @@
 import * as _dotenv from "dotenv/config";
 import { drizzle } from "drizzle-orm/postgres-js";
 import type { PostgresJsDatabase } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
 
 import { switchEnv } from "@chia/utils/config";
 
@@ -11,21 +10,15 @@ if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not set");
 }
 
-export const queryClient = postgres(process.env.DATABASE_URL);
-
-export const localQueryClient = postgres(process.env.LOCAL_DATABASE_URL ?? "");
-
-export const betaQueryClient = postgres(process.env.BETA_DATABASE_URL ?? "");
-
-export const db: DB = drizzle(queryClient, {
+export const db: DB = drizzle(process.env.DATABASE_URL, {
   schema,
 });
 
-export const localDb: DB = drizzle(localQueryClient, {
+export const localDb: DB = drizzle(process.env.LOCAL_DATABASE_URL ?? "", {
   schema,
 });
 
-export const betaDb: DB = drizzle(betaQueryClient, {
+export const betaDb: DB = drizzle(process.env.BETA_DATABASE_URL ?? "", {
   schema,
 });
 
@@ -43,11 +36,3 @@ export const getDB = (env?: string) =>
     beta: () => betaDb,
     local: () => localDb,
   });
-
-export const closeClient = async (env?: string) => {
-  await switchEnv(env, {
-    prod: async () => await queryClient.end(),
-    beta: async () => await betaQueryClient.end(),
-    local: async () => await localQueryClient.end(),
-  });
-};
