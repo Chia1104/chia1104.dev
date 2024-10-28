a69fea6591fa42f7e8bcdff3b8eb22b1301119b6
Chia1104 <yuyuchia7423@gmail.com>
2024-10-28 11:48:24 +0800

    fix: fix some lint issue

--- apps/dash/src/app/api/auth/[...nextauth]/route.ts
+++ apps/dash/src/app/api/auth/[...nextauth]/route.ts
@@ -2,8 +2,6 @@ import type { NextRequest } from "next/server";
 
 import { handlers } from "@chia/auth";
 
-// @ts-expect-error - [INTERNALS]
 export const GET = (req: NextRequest) => handlers.GET(req);
 
-// @ts-expect-error - [INTERNALS]
 export const POST = (req: NextRequest) => handlers.POST(req);
