c05e2b7fd262ad5b4b830965151aa852a11643aa
Chia1104 <yuyuchia7423@gmail.com>
2024-11-28 12:49:57 +0800

    [skip ci]test(api): set retry count

--- packages/api/spotify/index.spec.ts
+++ packages/api/spotify/index.spec.ts
@@ -7,15 +7,27 @@ vi.mock("server-only", () => {
 });
 
 describe("test spotify api", () => {
-  test("getPlayList", async () => {
-    const result = await getPlayList({
-      playlistId: "4cPPG7mh2a8EZ2jlhJfj9u",
-    });
-    expect(result).toBeDefined();
-  });
+  test(
+    "getPlayList",
+    {
+      retry: 3,
+    },
+    async () => {
+      const result = await getPlayList({
+        playlistId: "4cPPG7mh2a8EZ2jlhJfj9u",
+      });
+      expect(result).toBeDefined();
+    }
+  );
 
-  test("getNowPlaying", async () => {
-    const result = await getNowPlaying();
-    expect(result).toBeDefined();
-  });
+  test(
+    "getNowPlaying",
+    {
+      retry: 3,
+    },
+    async () => {
+      const result = await getNowPlaying();
+      expect(result).toBeDefined();
+    }
+  );
 });
