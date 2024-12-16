401c7d4b12130b210e670beb8da652e544a62a47
Chia1104 <yuyuchia7423@gmail.com>
2024-12-16 13:32:39 +0800

    Revert "fix(www): get locale from params"
    This reverts commit 781e33c17b60a5c4c7eb586ab2d7679c60bce564.

--- apps/www/src/app/[locale]/template.tsx
+++ apps/www/src/app/[locale]/template.tsx
@@ -1,18 +1,12 @@
 import type { ReactNode } from "react";
 
-import { getTimeZone } from "next-intl/server";
+import { getTimeZone, getLocale } from "next-intl/server";
 
 import { initDayjs } from "@/utils/dayjs";
-import { PageParamsWithLocale } from "@/utils/i18n";
+import type { I18N } from "@/utils/i18n";
 
-const Template = async ({
-  children,
-  params,
-}: {
-  children: ReactNode;
-  params: PageParamsWithLocale;
-}) => {
-  const { locale } = await params;
+const Template = async ({ children }: { children: ReactNode }) => {
+  const locale = (await getLocale()) as I18N;
   const timeZone = await getTimeZone();
   initDayjs(locale, timeZone);
   return children;
