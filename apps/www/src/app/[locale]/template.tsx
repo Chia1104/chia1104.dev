781e33c17b60a5c4c7eb586ab2d7679c60bce564
Chia1104 <yuyuchia7423@gmail.com>
2024-12-16 13:25:26 +0800

    fix(www): get locale from params

--- apps/www/src/app/[locale]/template.tsx
+++ apps/www/src/app/[locale]/template.tsx
@@ -1,12 +1,18 @@
 import type { ReactNode } from "react";
 
-import { getTimeZone, getLocale } from "next-intl/server";
+import { getTimeZone } from "next-intl/server";
 
 import { initDayjs } from "@/utils/dayjs";
-import type { I18N } from "@/utils/i18n";
+import { PageParamsWithLocale } from "@/utils/i18n";
 
-const Template = async ({ children }: { children: ReactNode }) => {
-  const locale = (await getLocale()) as I18N;
+const Template = async ({
+  children,
+  params,
+}: {
+  children: ReactNode;
+  params: PageParamsWithLocale;
+}) => {
+  const { locale } = await params;
   const timeZone = await getTimeZone();
   initDayjs(locale, timeZone);
   return children;
