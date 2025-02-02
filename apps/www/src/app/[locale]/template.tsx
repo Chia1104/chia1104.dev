e5587b160ac7050e5dcd5e82184639c148594450
Chia1104 <yuyuchia7423@gmail.com>
2025-02-03 02:24:43 +0800

    fix(www): remove client init dayjs

--- apps/www/src/app/[locale]/template.tsx
+++ apps/www/src/app/[locale]/template.tsx
@@ -2,7 +2,6 @@ import type { ReactNode } from "react";
 
 import { getTimeZone, getLocale } from "next-intl/server";
 
-import { InitDayjs } from "@/components/commons/init-dayjs";
 import { initDayjs } from "@/utils/dayjs";
 import type { I18N } from "@/utils/i18n";
 
@@ -10,12 +9,7 @@ const Template = async ({ children }: { children: ReactNode }) => {
   const locale = (await getLocale()) as I18N;
   const timeZone = await getTimeZone();
   initDayjs(locale, timeZone);
-  return (
-    <>
-      <InitDayjs />
-      {children}
-    </>
-  );
+  return <>{children}</>;
 };
 
 export default Template;
