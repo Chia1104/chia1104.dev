fd58f0733aefb0f2840b73971b3af744f90bc7e1
Chia1104 <yuyuchia7423@gmail.com>
2025-03-08 22:34:49 +0800

    fix(dash): invalidate project list data

--- apps/dash/src/app/(workspace)/projects/@create/default.tsx
+++ apps/dash/src/app/(workspace)/projects/@create/default.tsx
@@ -5,10 +5,13 @@ import { useTransitionRouter } from "next-view-transitions";
 
 import CreateForm from "@/components/projects/create-form";
 import { useOrganizationStore } from "@/store/organization.store";
+import { api } from "@/trpc/client";
 
 const Default = () => {
   const { currentOrgId } = useOrganizationStore((state) => state);
   const router = useTransitionRouter();
+  const utils = api.useUtils();
+
   return (
     <Card className="w-full max-w-md self-center">
       <CardHeader className="flex flex-col items-center gap-2">
@@ -16,7 +19,10 @@ const Default = () => {
       </CardHeader>
       <CreateForm
         organizationId={currentOrgId}
-        onSuccess={() => router.refresh()}
+        onSuccess={async () => {
+          router.refresh();
+          await utils.organization.getProjectsWithMeta.invalidate();
+        }}
       />
     </Card>
   );
