50266b2b63990d33eb2d7adf90a4665bc2ea92f2
Chia1104 <yuyuchia7423@gmail.com>
2025-03-14 11:33:41 +0800

    fix(www): catch nested RSC errors

--- apps/www/src/instrumentation.ts
+++ apps/www/src/instrumentation.ts
@@ -1,3 +1,7 @@
+import * as Sentry from "@sentry/nextjs";
+
+export const onRequestError = Sentry.captureRequestError;
+
 export async function register() {
   if (process.env.NEXT_RUNTIME === "client") {
     await import("../sentry.client.config");
