6346c61f2dbaaac44bf3fc6ee09e56350c79c303
Chia1104 <yuyuchia7423@gmail.com>
2025-04-24 10:47:48 +0800

    fix: update dependencies

--- apps/dash/src/components/feed/edit-fields.tsx
+++ apps/dash/src/components/feed/edit-fields.tsx
@@ -306,6 +306,7 @@ export const MetadataFields = () => {
                     labelPlacement="outside"
                     className="w-full"
                     label="Create"
+                    // @ts-expect-error - `@internationalized/date` version conflict
                     value={
                       field.value
                         ? parseDate(
@@ -317,6 +318,7 @@ export const MetadataFields = () => {
                         : null
                     }
                     onChange={(date) => {
+                      // @ts-expect-error - `@internationalized/date` version conflict
                       field.onChange(dayjs(date?.toString()).valueOf());
                     }}
                   />
@@ -337,6 +339,7 @@ export const MetadataFields = () => {
                       labelPlacement="outside"
                       className="w-full"
                       label="Update"
+                      // @ts-expect-error - `@internationalized/date` version conflict
                       value={
                         field.value
                           ? parseDate(
@@ -348,6 +351,7 @@ export const MetadataFields = () => {
                           : null
                       }
                       onChange={(date) => {
+                        // @ts-expect-error - `@internationalized/date` version conflict
                         field.onChange(dayjs(date?.toString()).valueOf());
                       }}
                     />
