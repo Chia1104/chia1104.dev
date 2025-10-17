1b7d0265660f92680154e2160990a6fdad313396
Chia1104 <yuyuchia7423@gmail.com>
2025-10-18 02:36:25 +0800

    fix(www): fix wrong effect

--- apps/www/src/components/commons/background.tsx
+++ apps/www/src/components/commons/background.tsx
@@ -1,23 +1,30 @@
 "use client";
 
 import type { FC } from "react";
+import { useState, useEffect } from "react";
 
 import { useIsMounted } from "usehooks-ts";
 
 import Spotlight from "@chia/ui/spotlight";
 import useTheme from "@chia/ui/utils/use-theme";
 
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
 const Background: FC = () => {
   const { isDarkMode } = useTheme();
   const isMounted = useIsMounted();
+  const [isOK, setIsOK] = useState(false);
+
+  useEffect(() => {
+    void delay(500).then(() => isMounted() && setIsOK(true));
+  }, [isMounted]);
+
   return (
     <>
       <Spotlight
         className="fixed -top-40 left-0 -z-40 md:-top-20 md:left-60"
         fill={
-          isDarkMode && isMounted()
-            ? "rgba(255, 255, 255, 0.7)"
-            : "rgba(0, 0, 0, 0.5)"
+          isDarkMode && isOK ? "rgba(255, 255, 255, 0.7)" : "rgba(0, 0, 0, 0.5)"
         }
       />
       <div className="c-background" />
