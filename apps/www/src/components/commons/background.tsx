46c15d1787b9edd3b3581ea4a5f44277e6336e96
Chia1104 <yuyuchia7423@gmail.com>
2025-10-18 02:24:59 +0800

    fix(ui): remove unused function

--- apps/www/src/components/commons/background.tsx
+++ apps/www/src/components/commons/background.tsx
@@ -1,23 +1,26 @@
 "use client";
 
 import type { FC } from "react";
-
-import { useIsMounted } from "usehooks-ts";
+import { useState, useEffect } from "react";
 
 import Spotlight from "@chia/ui/spotlight";
 import useTheme from "@chia/ui/utils/use-theme";
 
 const Background: FC = () => {
+  const [isOK, setIsOK] = useState(false);
   const { isDarkMode } = useTheme();
-  const isMounted = useIsMounted();
+  useEffect(() => {
+    const id = setInterval(() => {
+      setIsOK(true);
+    }, 500);
+    return () => clearInterval(id);
+  }, []);
   return (
     <>
       <Spotlight
         className="fixed -top-40 left-0 -z-40 md:-top-20 md:left-60"
         fill={
-          isDarkMode && isMounted()
-            ? "rgba(255, 255, 255, 0.7)"
-            : "rgba(0, 0, 0, 0.5)"
+          isDarkMode && isOK ? "rgba(255, 255, 255, 0.7)" : "rgba(0, 0, 0, 0.5)"
         }
       />
       <div className="c-background" />
