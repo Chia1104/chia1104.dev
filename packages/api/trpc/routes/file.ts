ba08b475cef1c0737150eb46c9cc2f8abc529680
Chia1104 <yuyuchia7423@gmail.com>
2025-11-11 02:31:55 +0800

    chore(api): file routes rename

--- packages/api/trpc/routes/file.ts
+++ packages/api/trpc/routes/file.ts
@@ -3,7 +3,7 @@ import { TRPCError } from "@trpc/server";
 import { adminProcedureWithACL, createTRPCRouter } from "../trpc";
 
 export const fileRouter = createTRPCRouter({
-  uploadFile: adminProcedureWithACL({
+  prepareUploadFile: adminProcedureWithACL({
     file: ["upload", "file.upload"],
   }).mutation(() => {
     throw new TRPCError({
