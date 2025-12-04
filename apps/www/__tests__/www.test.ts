07aa8df8c2020a490d17318c2fdf1d714847deb7
Chia1104 <yuyuchia7423@gmail.com>
2025-12-05 04:27:39 +0800

    ci: update vitest config

---
+++ apps/www/__tests__/www.test.ts
@@ -0,0 +1,5 @@
+describe("www", () => {
+  it("should be ok", () => {
+    expect(1).toBe(1);
+  });
+});
--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     passWithNoTests: true,
-    projects: ["apps/*", "packages/*"],
+    projects: ["apps/www", "apps/dash", "packages/*"],
     coverage: {
       reporter: ["lcov", "html"],
       provider: "v8",
