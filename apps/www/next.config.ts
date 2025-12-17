50ab1e41f7733f52118f7ab84204b8dc1a1d8ca7
Chia1104 <yuyuchia7423@gmail.com>
2025-12-18 04:09:49 +0800

    fix(www): enable sentry

--- apps/www/next.config.ts
+++ apps/www/next.config.ts
@@ -1,5 +1,5 @@
 import withBundleAnalyzerImport from "@next/bundle-analyzer";
-// import { withSentryConfig as withSentryConfigImport } from "@sentry/nextjs";
+import { withSentryConfig as withSentryConfigImport } from "@sentry/nextjs";
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
@@ -108,15 +108,17 @@ const nextComposePlugins = plugins.reduce(
   nextConfig
 );
 
-// export default withSentryConfigImport(withNextIntl(nextComposePlugins), {
-//   org: process.env.SENTRY_ORG,
-//   project: process.env.SENTRY_PROJECT,
-//   authToken: process.env.SENTRY_AUTH_TOKEN,
-//   silent: true,
-//   disableLogger: true,
-//   sourcemaps: {
-//     deleteSourcemapsAfterUpload: true,
-//   },
-// });
-
-export default withNextIntl(nextComposePlugins);
+export default withSentryConfigImport(withNextIntl(nextComposePlugins), {
+  org: process.env.SENTRY_ORG,
+  project: process.env.SENTRY_PROJECT,
+  authToken: process.env.SENTRY_AUTH_TOKEN,
+  silent: true,
+  sourcemaps: {
+    deleteSourcemapsAfterUpload: true,
+  },
+  webpack: {
+    treeshake: {
+      removeDebugLogging: true,
+    },
+  },
+});
