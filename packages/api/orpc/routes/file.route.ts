92667e758706bc22c3ae83931e58ba23f66398cd
Chia1104 <yuyuchia7423@gmail.com>
2026-01-15 06:20:51 +0800

    feat(api): s3 put object with sha256 checksum

--- packages/api/orpc/routes/file.route.ts
+++ packages/api/orpc/routes/file.route.ts
@@ -1,6 +1,10 @@
+import GitHubSlugger from "github-slugger";
+
 import { s3Service } from "../../s3/s3.service";
 import { adminGuard } from "../guards/admin.guard";
 import { contractOS } from "../utils";
+
+const slugger = new GitHubSlugger();
 
 export const createSignedUrlForUploadRoute = contractOS.file[
   "signed-url:create"
@@ -8,7 +12,12 @@ export const createSignedUrlForUploadRoute = contractOS.file[
   .use(adminGuard())
   .handler(async (opts) => {
     try {
-      const url = await s3Service.createSignedUrlForUpload(opts.input.key);
+      const name = `${opts.input.area}/${slugger.slug(opts.input.key)}-${crypto.randomUUID()}`;
+      const { url } = await s3Service.createSignedUrlForUpload(name, {
+        sha256Checksum: opts.input.sha256Checksum,
+        type: opts.input.type,
+        size: opts.input.size,
+      });
       return { url };
     } catch {
       throw opts.errors.INTERNAL_SERVER_ERROR();
