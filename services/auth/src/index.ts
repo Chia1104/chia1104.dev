aaff6aba4cdb3391e43a365a28a0c34d2336c1a6
Chia1104 <yuyuchia7423@gmail.com>
2026-01-20 22:46:55 +0800

    feat(auth-service): auth service dockerfile

--- services/auth/src/index.ts
+++ services/auth/src/index.ts
@@ -4,7 +4,14 @@ import { auth } from "@chia/auth";
 
 import { env } from "./env";
 
-const app = new Elysia().mount(auth.handler).listen(env.PORT);
+const app = new Elysia()
+  .get("/health", () => {
+    return {
+      status: "ok",
+    };
+  })
+  .mount(auth.handler)
+  .listen(env.PORT);
 
 console.log(
   `🦊 Auth Service is running at ${app.server?.protocol}://${app.server?.hostname}:${app.server?.port}`
