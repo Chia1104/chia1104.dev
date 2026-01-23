286c4170c7bab7d3bf4ac32bd97ce741bba6831a
Chia1104 <yuyuchia7423@gmail.com>
2026-01-23 23:24:58 +0800

    perf(www): remove canvas fadein

--- apps/www/src/components/about/location-hero.tsx
+++ apps/www/src/components/about/location-hero.tsx
@@ -4,7 +4,6 @@ import { useTranslations } from "next-intl";
 
 import Card from "@chia/ui/card";
 import { ErrorBoundary } from "@chia/ui/error-boundary";
-import FadeIn from "@chia/ui/fade-in";
 
 import Location from "@/components/about/location";
 
@@ -17,26 +16,24 @@ export const LocationHero = () => {
         {t("title")} <span className="i-mdi-location size-7" />
       </h2>
       <p>{t("description")}</p>
-      <FadeIn className="flex w-full justify-center">
-        <Card className="relative flex h-[300px] w-full justify-center p-2 pt-10 sm:h-[400px]">
-          <ErrorBoundary>
-            <span className="pointer-events-none whitespace-pre-wrap bg-linear-to-b from-black to-gray-300/80 bg-clip-text text-center text-5xl font-semibold leading-none text-transparent sm:text-6xl dark:from-white dark:to-slate-900/10">
-              {tProfile("location")}
-            </span>
-            <div className="absolute left-1/2 top-24 mx-auto aspect-square w-fit max-w-[600px] translate-x-[-50%]">
-              <Location
-                cobeOptions={{
-                  opacity: 0.9,
-                }}
-                className="size-[400px] sm:size-[600px]"
-                location={[24.91571, 121.6739]}
-                width={600}
-                height={600}
-              />
-            </div>
-          </ErrorBoundary>
-        </Card>
-      </FadeIn>
+      <Card className="relative flex h-[300px] w-full justify-center p-2 pt-10 sm:h-[400px]">
+        <ErrorBoundary>
+          <span className="pointer-events-none whitespace-pre-wrap bg-linear-to-b from-black to-gray-300/80 bg-clip-text text-center text-5xl font-semibold leading-none text-transparent sm:text-6xl dark:from-white dark:to-slate-900/10">
+            {tProfile("location")}
+          </span>
+          <div className="absolute left-1/2 top-24 mx-auto aspect-square w-fit max-w-[600px] translate-x-[-50%]">
+            <Location
+              cobeOptions={{
+                opacity: 0.9,
+              }}
+              className="size-[400px] sm:size-[600px]"
+              location={[24.91571, 121.6739]}
+              width={600}
+              height={600}
+            />
+          </div>
+        </ErrorBoundary>
+      </Card>
     </>
   );
 };
