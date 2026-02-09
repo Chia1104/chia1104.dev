cb348768555a9447fb3258dc5bb28d65426bbe6a
Chia1104 <yuyuchia7423@gmail.com>
2026-02-09 23:00:14 +0800

    fix(www): fix post og locale

--- apps/www/src/app/[locale]/(blog)/[type]/[slug]/opengraph-image.tsx
+++ apps/www/src/app/[locale]/(blog)/[type]/[slug]/opengraph-image.tsx
@@ -1,10 +1,12 @@
 import { ImageResponse } from "next/og";
 import { NextResponse } from "next/server";
 
+import { Locale as DBLocale } from "@chia/db/types";
 import OpenGraph from "@chia/ui/open-graph";
 import dayjs from "@chia/utils/day";
 import { errorGenerator } from "@chia/utils/server";
 
+import { Locale } from "@/libs/utils/i18n";
 import { getFeedBySlug } from "@/services/feeds.service";
 
 export const alt = "Blog";
@@ -17,10 +19,10 @@ export const contentType = "image/png";
 export default async function og({
   params,
 }: {
-  params: Promise<{ slug: string }>;
+  params: Promise<{ slug: string; locale: string }>;
 }) {
-  const { slug } = await params;
-  const post = await getFeedBySlug(slug);
+  const { slug, locale } = await params;
+  const post = await getFeedBySlug(slug, localeResolver(locale));
   const translation = post?.translations[0];
   if (!translation) {
     return NextResponse.json(errorGenerator(404), { status: 404 });
@@ -43,4 +45,15 @@ export default async function og({
       status: 200,
     }
   );
+}
+
+function localeResolver(locale: string) {
+  switch (locale) {
+    case Locale.EN:
+      return DBLocale.En;
+    case Locale.ZH_TW:
+      return DBLocale.zhTW;
+    default:
+      return DBLocale.zhTW;
+  }
 }
