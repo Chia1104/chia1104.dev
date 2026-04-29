69909a330284cb621cfb6a3cedcab04158f9e1ca
Chia1104 <yuyuchia7423@gmail.com>
2026-04-29 13:18:36 +0800

    fix(www): streamline CacheFeeds and enhance FeedList for no-content state

--- apps/www/src/app/[locale]/(blog)/[type]/page.tsx
+++ apps/www/src/app/[locale]/(blog)/[type]/page.tsx
@@ -6,8 +6,6 @@ import { ErrorBoundary } from "@sentry/nextjs";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { getTranslations } from "next-intl/server";
 
-import Image from "@chia/ui/image";
-import ImageZoom from "@chia/ui/image-zoom";
 import dayjs from "@chia/utils/day";
 
 import FeedList from "@/components/blog/feed-list";
@@ -45,9 +43,8 @@ const CacheFeeds = async ({
   locale: Locale;
 }) => {
   const formattedType = type === "posts" ? "post" : "note";
-  const t = await getTranslations(`blog.${type}`);
 
-  const feeds = await queryClient.fetchInfiniteQuery(
+  await queryClient.prefetchInfiniteQuery(
     orpc.feeds["admin-list"].infiniteOptions({
       input: () => ({
         limit,
@@ -65,10 +62,7 @@ const CacheFeeds = async ({
     })
   );
 
-  const hasFeeds =
-    Array.isArray(feeds?.pages[0]?.items) && feeds?.pages[0]?.items.length > 0;
-
-  return hasFeeds ? (
+  return (
     <HydrationBoundary state={dehydrate(queryClient)}>
       <FeedList
         type={formattedType}
@@ -82,22 +76,6 @@ const CacheFeeds = async ({
         }}
       />
     </HydrationBoundary>
-  ) : (
-    <div className="c-bg-third relative flex flex-col items-center justify-center overflow-hidden rounded-lg px-5 py-10">
-      <p>{t("no-content")}</p>
-      <ImageZoom>
-        <div className="not-prose relative aspect-square w-[100px]">
-          <Image
-            src="https://storage.chia1104.dev/memo.png"
-            alt="memo"
-            className="object-cover"
-            fill
-            loading="lazy"
-          />
-        </div>
-      </ImageZoom>
-      <div className="dark:c-bg-gradient-purple-to-pink c-bg-gradient-yellow-to-pink absolute -z-40 size-full opacity-50 blur-3xl" />
-    </div>
   );
 };
 
