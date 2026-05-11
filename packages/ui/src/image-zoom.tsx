ab864d4ad24f0e26678e01bfca015c927cd71ebc
Chia1104 <yuyuchia7423@gmail.com>
2026-05-11 20:40:06 +0800

    fix(ui): image-zoom ssr support

--- packages/ui/src/image-zoom.tsx
+++ packages/ui/src/image-zoom.tsx
@@ -1,13 +1,9 @@
 "use client";
 
-import dynamic from "next/dynamic";
 import type { FC } from "react";
 
 import type { UncontrolledProps } from "react-medium-image-zoom";
-
-const Zoom = dynamic(() => import("react-medium-image-zoom"), {
-  ssr: false,
-});
+import Zoom from "react-medium-image-zoom";
 
 const ImageZoom: FC<UncontrolledProps> = (props) => {
   return <Zoom zoomMargin={40} {...props} />;
