ee964b5bb2047a4da6808ddbcfa61095d0cb5185
Chia1104 <yuyuchia7423@gmail.com>
2026-07-27 17:35:36 +0800

    fix(service): insure `@workflow/world-postgres` in nitro bundle

--- apps/service/plugins/start-pg-world.ts
+++ apps/service/plugins/start-pg-world.ts
@@ -5,8 +5,8 @@ export default definePlugin(async () => {
   if (process.env.WORKFLOW_TARGET_WORLD === "@workflow/world-postgres") {
     // Dynamic import to avoid edge runtime bundling issues
     console.log("Starting Postgres World...");
-    const { getWorld } = await import("workflow/runtime");
-    await getWorld().start?.();
+    const { createWorld } = await import("@workflow/world-postgres");
+    await createWorld().start?.();
     console.log("Postgres World started");
   }
 });
