f83c02db4f49b021b3dae68040493a8d464094ac
Chia1104 <yuyuchia7423@gmail.com>
2026-09-15 16:53:05 +0800

    ci: add observability package to backend watch patterns

--- .railway/config.ts
+++ .railway/config.ts
@@ -26,6 +26,7 @@ const backendPackageWatchPatterns = [
   "/packages/ui/**",
   "/packages/utils/**",
   "/packages/workflow-control/**",
+  "/packages/observability/**",
 ];
 
 export const serviceWatchPatterns = [
